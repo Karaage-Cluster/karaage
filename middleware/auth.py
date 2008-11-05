7c2a07c373750c2324963fc3935e14b1a313fafc
Sam Morrison <sam@vpac.org>
2008-11-05 03:11:27 +0000

    added apache auth middleware

---
+++ middleware/auth.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import login
+from django.http import HttpResponseForbidden
+from django.contrib.auth.models import User
+
+class ApacheSiteLogin:
+    "This middleware logs a user in using the REMOTE_USER header from apache"
+    def process_request(self, request):
+
+        if request.user.is_anonymous():
+            try:
+                user = User.objects.get(username__exact=request.META['REMOTE_USER'])
+                user.backend = 'django_common.backends.auth.LDAPBackend'
+                login(request, user)
+            except:
+                return HttpResponseForbidden("<h1>Failed log in.</h1><p>Try to refresh page</p>")
+
+
+
+
+
+
+
+
+
+
