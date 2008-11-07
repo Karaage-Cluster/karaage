e6b4d6f7a1acd06a32ce7618c0aaada2f88649a7
Sam Morrison <sam@vpac.org>
2008-11-07 03:22:07 +0000

    include self in method

--- graphs/base.py
+++ graphs/base.py
@@ -5,7 +5,7 @@ Module for abstract graph generation base classes.
 
 class GraphGenerator(object):
 
-    def gen_project_graph(project, start, end, machine_category):
+    def gen_project_graph(self, project, start, end, machine_category):
         """Generates a bar graph for a project
     
         Keyword arguments:
@@ -17,7 +17,7 @@ class GraphGenerator(object):
         """
         raise NotImplementedError
 
-    def gen_institutes_pie(start, end, machine_category):
+    def gen_institutes_pie(self, start, end, machine_category):
         """Generates a pie graph showing all primary institutes usage
     
         Keyword arguments:
@@ -29,14 +29,14 @@ class GraphGenerator(object):
         raise NotImplementedError
 
 
-    def gen_quota_graph():
+    def gen_quota_graph(self):
         """Generates a pie graph for all primary institutes quota       
         """
         raise NotImplementedError
 
 
 
-    def gen_trend_graph(start, end, machine_category):
+    def gen_trend_graph(self, start, end, machine_category):
         """Generates a bar graph showing the trend usage for a machine category
         
         Keyword arguments:
@@ -48,7 +48,7 @@ class GraphGenerator(object):
         raise NotImplementedError
     
     
-    def gen_institute_bar(institute, start, end, machine_category): 
+    def gen_institute_bar(self, institute, start, end, machine_category): 
         """Generates a bar graph showing the trend usage for an institute
 
         Keyword arguments:
