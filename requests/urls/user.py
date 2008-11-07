6a595a646dc530330bb0d7b2e289f9ed60789b56
Sam Morrison <sam@vpac.org>
2008-11-07 02:35:41 +0000

    url name

--- requests/urls/user.py
+++ requests/urls/user.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('karaage.requests.views.user',
     url(r'^$', 'user_registration', name='user_registration'),
     url(r'^cancel/(?P<user_request_id>\d+)/$', 'cancel_user_registration'),
     url(r'^choose_project/$', 'choose_project', name='user_choose_project'),
-    url(r'^created/(?P<user_request_id>\d+)/$', 'account_created', name='user_account_created'),
+    url(r'^created/(?P<user_request_id>\d+)/$', 'account_created', name='kg_user_account_pending'),
                         
     url(r'^approve/(?P<user_request_id>\d+)/$', 'approve_person', name='user_approve_account'),
     url(r'^reject/(?P<user_request_id>\d+)/$', 'reject_person', name='user_reject_account'),
