27037c71c46eb644306e13bee68ddadc12817d83
Sam Morrison <sam@vpac.org>
2009-06-03 01:31:59 +0000

    show when a project is still to be approved

--- projects/views/admin.py
+++ projects/views/admin.py
@@ -77,6 +77,10 @@ def project_detail(request, project_id):
 
     project = get_object_or_404(Project, pk=project_id)
     user_list = Person.active.all()
+    
+    requestor = False
+    if project.projectcreaterequest_set.count() > 0:
+        requestor = True
 
     if request.REQUEST.has_key('showall'):
         showall = True
