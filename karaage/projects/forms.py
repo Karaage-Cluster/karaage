a0ef181a77c12225b3161f7c32f9c9f90b61862d
Sam Morrison <sam@vpac.org>
2010-05-03 16:58:04 +1000

    Removed old project form class

--- karaage/projects/forms.py
+++ karaage/projects/forms.py
@@ -45,63 +45,6 @@ class ProjectForm(forms.ModelForm):
         fields = ('name', 'institute', 'leader', 'description', 'start_date', 'end_date', 'additional_req', 'machine_categories', 'machine_category')
 
 
-class ProjectForm2(forms.Form):
-    pid = forms.CharField(max_length=10, required=False, help_text="If left blank the next available pid will be used")
-    name = forms.CharField(label='Project Title', widget=forms.TextInput(attrs={ 'size':60 }))
-    description = forms.CharField(widget=forms.Textarea(attrs={'class':'vLargeTextField', 'rows':10, 'cols':40 }), required=False)
-    institute = forms.ModelChoiceField(queryset=Institute.valid.all())
-    additional_req = forms.CharField(widget=forms.Textarea(attrs={'class':'vLargeTextField', 'rows':10, 'cols':40 }), required=False)
-    is_expertise = forms.BooleanField(required=False, help_text=u"Is this a current VPAC funded Expertise or Education Project?")
-    leader = forms.ModelChoiceField(queryset=Person.active.all())
-    start_date = forms.DateField(widget=AdminDateWidget)
-    end_date = forms.DateField(widget=AdminDateWidget, required=False)
-    machine_categories = forms.ModelMultipleChoiceField(queryset=MachineCategory.objects.all(), widget=forms.CheckboxSelectMultiple)
-    
-    def save(self, p=None):
-        data = self.cleaned_data
-
-        if p is None:
-            p = Project()
-            if data['pid']:
-                p.pid = data['pid']
-            else:
-                p.pid = get_new_pid(data['institute'], data['is_expertise'])
-            p.is_active = True
-            p.is_approved = True
-            p.date_approved = datetime.datetime.today()
-            approver = get_current_user()
-            p.approved_by = approver.get_profile()
-
-            log_object(get_current_user(), p, 1, 'Created')
- 
-        else:
-            log_object(get_current_user(), p, 2, 'Edited')
-            
-        p.name = data['name']
-        p.description = data['description']
-        p.institute = data['institute']
-        p.additional_req = data['additional_req']
-        p.is_expertise = data['is_expertise']
-        p.leader = data['leader']
-        p.start_date = data['start_date']
-        p.end_date = data['end_date']
-        p.machine_categories = data['machine_categories']
-        p.save()
-
-        return p
-            
-    def clean_pid(self):
-        if self.cleaned_data.get('pid'):
-            try:
-                project = Project.objects.get(pk=self.cleaned_data['pid'])
-            except:
-                return self.cleaned_data['pid']
-            raise forms.ValidationError(u'PID already exists')
-
-
-
-
-
 class UserProjectForm(forms.Form):
     """
     This form is for people who have an account and want to start a new project
