31bf47b716e651e60b05a86940ffbca23e711d28
Sam Morrison <sam@vpac.org>
2010-07-28 09:23:47 +1000

    Remove django 1.1 DB settings. LOGIN_URL and LOGIN_REDIRECT_URL belong in karaage-admin

--- conf/global_settings.py
+++ conf/global_settings.py
@@ -8,13 +8,13 @@
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+# Will recieve error reports if something goes wrong
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
 
 MANAGERS = ADMINS
 
-# Multi DB Syntax (Django >= 1.2)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
@@ -26,23 +26,10 @@ DATABASES = {
     }
 }
 
-# Single DB (Django < 1.2)
-DATABASE_ENGINE = '' # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '' # Or path to database file if using sqlite3.
-DATABASE_USER = '' # Not used with sqlite3
-DATABASE_PASSWORD = '' # Not used with sqlite3.
-DATABASE_HOST = '' # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = '' # Set to empty string for default. Not used with sqlite3.
-
-
 # Defaults used for error reports
 SERVER_EMAIL = 'karaage@example.org'
 EMAIL_HOST = 'localhost'
 EMAIL_SUBJECT_PREFIX = '[Karaage] - '
-
-# Default URLs for logging in
-LOGIN_URL="/accounts/login/"
-LOGIN_REDIRECT_URL="/"
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -58,7 +45,7 @@ LANGUAGE_CODE = 'en-au'
 SITE_ID = 1
 
 # Unique key used for storing session data etc.
-# To generate this key run kg_set_secret_key
+# To generate this key run /usr/sbin/kg_set_secret_key
 SECRET_KEY = ''
 
 ###
