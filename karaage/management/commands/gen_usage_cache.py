14e768fba587c2142fdd2452fb14d0ff28f2bab3
Sam Morrison <sam@vpac.org>
2010-08-27 16:53:20 +1000

    Added License to command files

--- karaage/management/commands/gen_usage_cache.py
+++ karaage/management/commands/gen_usage_cache.py
@@ -1,11 +1,31 @@
+# Copyright 2007-2010 VPAC
+#
+# This file is part of Karaage.
+#
+# Karaage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Karaage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Karaage  If not, see <http://www.gnu.org/licenses/>.
+
+
 from django.core.management.base import BaseCommand, CommandError
 
-PERIODS = [ 7, 90, 365 ]
+import datetime
 
 from karaage.machines.models import Machine
 from karaage.projects.models import Project
 from karaage.people.models import Institute, Person
-import datetime
+
+
+PERIODS = [ 7, 90, 365 ]
 
 def pop_project_cache(period):
     end = datetime.date.today()
