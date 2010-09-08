77b05182fc7a6130bf4cd71577def61b678c6f1e
Brian May <bmay@unimelb.edu.au>
2010-09-08 12:11:33 +1000

    Add AJAX project lookup method.

---
+++ karaage/projects/lookups.py
@@ -0,0 +1,28 @@
+from django.db.models import Q
+from django.utils.html import escape
+
+from karaage.projects.models import Project
+
+class ProjectLookup(object):
+
+    def get_query(self, q, request):
+        """ return a query set.  you also have access to request.user if needed """
+
+        if not request.user.is_staff:
+            return Project.objects.none()
+
+        return Project.objects.filter(Q(pid__icontains=q) | Q(name__icontains=q))
+
+    def format_result(self, p):
+        """ the search results display in the dropdown menu.  may contain html and multiple-lines. will remove any |  """
+        return escape(u"%s" % (p))
+
+    def format_item(self, p):
+        """ the display of a currently selected object in the area below the search box. html is OK """
+        return escape(u"%s" % (p))
+
+    def get_objects(self, ids):
+        """ given a list of ids, return the objects ordered as you would like them on the admin page.
+            this is for displaying the currently selected items (in the case of a ManyToMany field)
+        """
+        return ReportPerson.objects.filter(pk__in=ids)
