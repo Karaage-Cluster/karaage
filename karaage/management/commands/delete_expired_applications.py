350c9f7753301142548c8b9c5eb613d75e1a2418
Sam Morrison <sam@vpac.org>
2011-02-15 13:39:19 +1100

    Management command now deletes all applications that have been complete for 30 days

--- karaage/management/commands/delete_expired_applications.py
+++ karaage/management/commands/delete_expired_applications.py
@@ -18,23 +18,31 @@
 from django.core.management.base import BaseCommand, CommandError
 
 class Command(BaseCommand):
-    help = "Deletes expired applications in the NEW state"
+    help = "Deletes expired applications in the NEW state and applications that have been complete for a month"
     
     def handle(self, **options):
-        from karaage.applications.models import Application
+        from karaage.applications.models import Application, UserApplication, ProjectApplication
         import datetime
         now = datetime.datetime.now()
         
         verbose = int(options.get('verbosity'))
-        
+        # Delete all expired applications
         for application in Application.objects.filter(expires__lte=now, state=Application.NEW):
             if verbose >= 1:
                 print "Deleted expired application #%s" % application.id
             application.delete()
  
-        
-        for application in Application.objects.filter(state=Application.COMPLETE):
+        month_ago = now - datetime.timedelta(days=30)
+        # Delete all User applications that have been complete for 1 month
+        for application in UserApplication.objects.filter(state=Application.COMPLETE, complete_date__lte=month_ago):
             if verbose >= 1:
                 print "Deleted completed user application #%s" % application.id
+
+            application.delete()
+
+        # Delete all project applications that have been complete for 1 month
+        for application in ProjectApplication.objects.filter(state=Application.COMPLETE, complete_date__lte=month_ago): 
+            if verbose >= 1:
+                print "Deleted completed project application #%s" % application.id
 
             application.delete()
