a8c8558585efe6550b0a537ab7296223632e6d70
Sam Morrison <sam@vpac.org>
2011-05-25 11:02:11 +1000

    Improvements in the institute form

--- karaage/institutes/forms.py
+++ karaage/institutes/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-
-from karaage.people.models import Institute
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from karaage.people.models import Institute, Person
 
 class InstituteForm(forms.ModelForm):
+    delegate = forms.ModelChoiceField(queryset=Person.active.select_related(), required=False)
+    active_delegate = forms.ModelChoiceField(queryset=Person.active.select_related(), required=False)
+    sub_delegates = forms.ModelMultipleChoiceField(
+        queryset=Person.active.select_related(), required=False, 
+        widget=FilteredSelectMultiple('Sub Delegates', False))
+
 
     def clean_saml_entityid(self):
         if self.cleaned_data['saml_entityid'] == "":
