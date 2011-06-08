da6ce9f2947c4dcefaa98fb017dd98038dee229f
Sam Morrison <sam@vpac.org>
2011-06-08 16:34:56 +1000

    Fixed #2

--- karaage/software/urls/user.py
+++ karaage/software/urls/user.py
@@ -23,5 +23,6 @@ urlpatterns = patterns('karaage.software.views.user',
     url(r'^add/$', 'add_package_list', name='kg_software_list'),
     url(r'^add/(?P<package_id>\d+)/$', 'add_package'),
     url(r'^add/(?P<package_id>\d+)/print/$', 'license_txt'),
+    url(r'^(?P<package_id>\d+)/$', 'license_txt', name='kg_software_detail'),
 )
                         
