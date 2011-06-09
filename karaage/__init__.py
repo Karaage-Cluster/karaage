5c179b7562d23460b1388a3976c794bcb115ca60
Sam Morrison <sam@vpac.org>
2011-06-10 09:11:23 +1000

    New version 2.5.14

--- karaage/__init__.py
+++ karaage/__init__.py
@@ -16,7 +16,7 @@
 # along with Karaage  If not, see <http://www.gnu.org/licenses/>.
 
 
-VERSION = (2, 5, 13, 'final', 0)
+VERSION = (2, 5, 14, 'final', 0)
 
 def get_version():
     """ Return the current version"""
