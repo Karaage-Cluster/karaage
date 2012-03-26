a0dfe46db55c4515ce2f7952a94f8986b78eab71
Brian May <brian@vpac.org>
2012-03-26 11:28:09 +1100

    Update for latest django-ajax-selects.

--- karaage/projects/lookups.py
+++ karaage/projects/lookups.py
@@ -20,8 +20,9 @@ from django.utils.html import escape
 
 from karaage.projects.models import Project
 
+from ajax_select import LookupChannel
 
-class ProjectLookup(object):
+class ProjectLookup(LookupChannel):
 
     def get_query(self, q, request):
         """ return a query set.  you also have access to request.user if needed """
@@ -31,16 +32,14 @@ class ProjectLookup(object):
 
         return Project.objects.filter(Q(pid__icontains=q) | Q(name__icontains=q))
 
-    def format_result(self, p):
-        """ the search results display in the dropdown menu.  may contain html and multiple-lines. will remove any |  """
+    def get_result(self,obj):
+        u""" result is the simple text that is the completion of what the person typed """
+        return obj.pid
+
+    def format_match(self, p):
+        """ (HTML) formatted item for display in the dropdown """
         return escape(u"%s" % (p))
 
-    def format_item(self, p):
-        """ the display of a currently selected object in the area below the search box. html is OK """
+    def format_item_display(self, p):
+        """ (HTML) formatted item for displaying item in the selected deck area """
         return escape(u"%s" % (p))
-
-    def get_objects(self, ids):
-        """ given a list of ids, return the objects ordered as you would like them on the admin page.
-            this is for displaying the currently selected items (in the case of a ManyToMany field)
-        """
-        return Project.objects.filter(pk__in=ids)
