7bd4081865964432a2446ebc7ce74325c7886aa7
Sam Morrison <sam@vpac.org>
2012-05-01 15:09:47 +1000

    Require permissions to edit machine category, fixes #174

--- karaage/machines/urls.py
+++ karaage/machines/urls.py
@@ -16,15 +16,16 @@
 # along with Karaage  If not, see <http://www.gnu.org/licenses/>.
 
 from django.conf.urls.defaults import *
-
+from django.contrib.auth.decorators import permission_required
+from django.views.generic.create_update import update_object, create_object
 from models import Machine, MachineCategory
 
 
-urlpatterns = patterns('django.views.generic.create_update',                        
-    url(r'^add/$', 'create_object', {'model': Machine}),    
-    url(r'^(?P<object_id>\d+)/edit/$', 'update_object', {'model': Machine}),       
-    url(r'^categories/add/$', 'create_object', {'model': MachineCategory}, name='kg_machinecategory_add'),    
-    url(r'^categories/(?P<object_id>\d+)/edit/$', 'update_object', {'model': MachineCategory}, name='kg_machinecategory_edit'),
+urlpatterns = patterns('django.views.generic.create_update',
+    url(r'^add/$', permission_required('machines.add_machine')(create_object), {'model': Machine}),
+    url(r'^(?P<object_id>\d+)/edit/$', permission_required('machines.change_machine')(update_object), {'model': Machine}),
+    url(r'^categories/add/$', permission_required('machines.add_machinecategory')(create_object), {'model': MachineCategory}, name='kg_machinecategory_add'),
+    url(r'^categories/(?P<object_id>\d+)/edit/$', permission_required('machines.change_machinecategory')(update_object), {'model': MachineCategory}, name='kg_machinecategory_edit'),
 )
 
 urlpatterns += patterns('karaage.machines.views',
@@ -34,4 +35,3 @@ urlpatterns += patterns('karaage.machines.views',
     url(r'^(?P<machine_id>\d+)/user_accounts/$', 'machine_accounts'),
     url(r'^(?P<machine_id>\d+)/projects/$', 'machine_projects'),
 )
-                  
