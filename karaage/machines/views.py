6695c49b44ad23e880729e89819d0b82bea8ca36
Sam Morrison <sam@vpac.org>
2012-05-01 15:07:42 +1000

    Removed whitespace

--- karaage/machines/views.py
+++ karaage/machines/views.py
@@ -22,32 +22,35 @@ from karaage.machines.models import Machine, MachineCategory
 
 
 def index(request):
-
     category_list = MachineCategory.objects.all()
-
-    return render_to_response('machines/machine_list.html', locals(), context_instance=RequestContext(request))
+    return render_to_response(
+        'machines/machine_list.html',
+        {'category_list': category_list},
+        context_instance=RequestContext(request))
 
 
 def machine_detail(request, machine_id):
-
     machine = get_object_or_404(Machine, pk=machine_id)
-
     usage_list = machine.cpujob_set.all()[:5]
-
-    return render_to_response('machines/machine_detail.html', locals(), context_instance=RequestContext(request))
+    return render_to_response(
+        'machines/machine_detail.html',
+        {'machine': machine, 'usage_list': usage_list},
+        context_instance=RequestContext(request))
 
 
 def machine_accounts(request, machine_id):
-
     machine = get_object_or_404(Machine, pk=machine_id)
     user_accounts = machine.category.useraccount_set.filter(date_deleted__isnull=True)
-
-    return render_to_response('machines/machine_accounts.html', locals(), context_instance=RequestContext(request))
+    return render_to_response(
+        'machines/machine_accounts.html',
+        {'machine': machine, 'user_accounts': user_accounts},
+        context_instance=RequestContext(request))
 
 
 def machine_projects(request, machine_id):
-
     machine = get_object_or_404(Machine, pk=machine_id)
     project_list = machine.category.project_set.all()
-
-    return render_to_response('machines/machine_projects.html', locals(), context_instance=RequestContext(request))
+    return render_to_response(
+        'machines/machine_projects.html',
+        {'machine': machine, 'project_list': project_list},
+        context_instance=RequestContext(request))
