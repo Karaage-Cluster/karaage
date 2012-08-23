9d2345643f471730b548447d7bc0e412fe76936c
Brian May <brian@microcomaustralia.com.au>
2012-08-23 11:58:53 +1000

    Update TEMPLATE_CONTEXT_PROCESSORS for Django 1.4.

--- karaage/conf/settings.py
+++ karaage/conf/settings.py
@@ -69,7 +69,7 @@ TEMPLATE_DIRS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.static',
     'django.core.context_processors.media',
