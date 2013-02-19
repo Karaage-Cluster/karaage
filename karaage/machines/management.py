fcdcb5016f64d7a96f9cd746980b2d8850c88579
Brian May <brian@microcomaustralia.com.au>
2013-02-19 13:15:51 +1100

    Revert "Remove database initialization for machine_category."
    This reverts commit b336fb067de02454328c021e2fce8a254ab8bec5.
    
    This code is required for tests. The sql file isn't used????

--- karaage/machines/management.py
+++ karaage/machines/management.py
@@ -14,3 +14,22 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Karaage  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Creates the default MachineCategory object.
+"""
+
+from django.db.models import signals
+from karaage.machines.models import MachineCategory
+from karaage.machines import models as machines_app
+
+
+def create_default_machinecategory(app, created_models, verbosity, db, **kwargs):
+    if MachineCategory in created_models:
+        if verbosity >= 2:
+            print "Creating Default MachineCategory object"
+        mc = MachineCategory(name='Default')
+        mc.save(using=db)
+    MachineCategory.objects.clear_cache()
+
+signals.post_syncdb.connect(create_default_machinecategory, sender=machines_app)
