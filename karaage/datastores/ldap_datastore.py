Karaage-Cluster/karaage

commit 96fc5ae69bc0d1c0037e2223f0dccfcf6347abed

Fix initial passwords
For new accounts, not yet approved, the password is encrypted in a
openldap specific manner and stored in the user.password field. Once the
account is approved, Karaage copies the user.password field and uses it
to set the password. For people that have no accounts, Karaage requires
an LDAP entry in case the person is ever upgraded in the future to have
another account.

This creates a number of probems as described in
https://github.com/Karaage-Cluster/karaage/issues/198

However if we remove this kludge, we need some way of being able to set
the user's password when the user's account is approved (by
administrator) without storing it in plain text format.

This change requires the user set the account's password after the
account is created, not before as currently is the case.

This change also removes the requirement that every person must have an
LDAP entry. Only people with accounts require LDAP entries. Still need a
better way of dealing with existing people who already have LDAP
entries.

Fixes #198.

Change-Id: I5d21b43194522f3f572ff658b15fef5e815acf4b

karaage/datastores/ldap_datastore.py
@@ -26,88 +26,7 @@ def str_or_none(string):
     return string
 
 class PersonalDataStore(base.PersonalDataStore):
-    
-    def activate_user(self, person):
-        super(PersonalDataStore, self).activate_user(person)
-
-        p = ldap_schemas.person()
-        p.set_defaults()
-        p.uid = person.username
-        p.givenName = person.first_name
-        p.sn = person.last_name
-        p.telephoneNumber = str_or_none(person.telephone)
-        p.mail = str_or_none(person.email)
-        p.title = str_or_none(person.title)
-        p.o = person.institute.name
-        p.userPassword = str(person.user.password)
-        p.pre_create(master=None)
-        p.pre_save()
-        p.save()
-
-        # Person.create sets user.password to LDAP password that
-        # django doesn't understand. Need to reset it.
-        person.user.set_unusable_password()
-        person.user.save()
-
-    def delete_user(self, person):
-        super(PersonalDataStore, self).delete_user(person)
-        try:
-            p = ldap_schemas.person.objects.get(uid=person.username)
-            p.pre_delete()
-            p.delete()
-        except ldap_schemas.person.DoesNotExist:
-            pass
-
-    def update_user(self, person):
-        super(PersonalDataStore, self).update_user(person)
-        try:
-            p = ldap_schemas.account.objects.get(uid=person.username)
-        except ldap_schemas.account.DoesNotExist:
-            p = ldap_schemas.person.objects.get(uid=person.username)
-        p.givenName = person.first_name
-        p.sn = person.last_name
-        p.telephoneNumber = str_or_none(person.telephone)
-        p.mail = str_or_none(person.email)
-        p.title = str_or_none(person.title)
-        p.o = person.institute.name
-        p.pre_save()
-        p.save()
-
-    def lock_user(self, person):
-        super(PersonalDataStore, self).lock_user(person)
-        p = ldap_schemas.person.objects.get(uid=person.username)
-        p.lock()
-        p.pre_save()
-        p.save()
-
-    def unlock_user(self, person):
-        super(PersonalDataStore, self).unlock_user(person)
-        p = ldap_schemas.person.objects.get(uid=person.username)
-        p.unlock()
-        p.pre_save()
-        p.save()
-
-    def set_password(self, person, raw_password):
-        super(PersonalDataStore, self).set_password(person, raw_password)
-        p = ldap_schemas.person.objects.get(uid=person.username)
-        p.change_password(raw_password)
-        p.pre_save()
-        p.save()
-
-    def user_exists(self, username):
-        try:
-            ldap_schemas.person.objects.get(uid=username)
-            return True
-        except ldap_schemas.person.DoesNotExist:
-            return False
-
-    def create_password_hash(self, raw_password):
-        from placard.ldap_passwd import md5crypt
-        return '{crypt}%s' % md5crypt(raw_password)
-
-    def change_username(self, person, new_username):
-        p = ldap_schemas.person.objects.get(uid=person.username)
-        p.rename(uid=new_username)
+    pass
 
 
 class AccountDataStore(base.AccountDataStore):
@@ -117,8 +36,17 @@ class AccountDataStore(base.AccountDataStore):
 
         person = ua.user
 
-        luser = ldap_schemas.account.objects.convert(ldap_schemas.person).get(uid=ua.username)
+        luser = ldap_schemas.account()
         luser.set_defaults()
+        luser.uid = person.username
+        luser.givenName = person.first_name
+        luser.sn = person.last_name
+        luser.telephoneNumber = str_or_none(person.telephone)
+        luser.mail = str_or_none(person.email)
+        luser.title = str_or_none(person.title)
+        luser.o = person.institute.name
+        luser.userPassword = "" # FIXME
+
         luser.gidNumber = person.institute.gid
         luser.homeDirectory = settings.HOME_DIRECTORY % { 'default_project': ua.default_project.pid, 'uid': luser.uid }
         luser.loginShell = ua.shell
@@ -134,8 +62,17 @@ class AccountDataStore(base.AccountDataStore):
 
     def update_account(self, ua):
         super(AccountDataStore, self).update_account(ua)
+
+        person = ua.user
+
         luser = ldap_schemas.account.objects.get(uid=ua.username)
         luser.gidNumber = ua.user.institute.gid
+        luser.givenName = person.first_name
+        luser.sn = person.last_name
+        luser.telephoneNumber = str_or_none(person.telephone)
+        luser.mail = str_or_none(person.email)
+        luser.title = str_or_none(person.title)
+        luser.o = person.institute.name
         luser.pre_save()
         luser.save()
 
@@ -151,3 +88,22 @@ class AccountDataStore(base.AccountDataStore):
         luser.loginShell = shell
         luser.pre_save()
         luser.save()
+
+    def set_password(self, ua, raw_password):
+        super(AccountDataStore, self).set_password(ua, raw_password)
+        p = ldap_schemas.account.objects.get(uid=ua.username)
+        p.change_password(raw_password)
+        p.pre_save()
+        p.save()
+
+    def account_exists(self, username):
+        try:
+            ldap_schemas.account.objects.get(uid=username)
+            return True
+        except ldap_schemas.account.DoesNotExist:
+            return False
+
+    def change_username(self, ua, new_username):
+        super(AccountDataStore, self).change_username(ua, new_username)
+        p = ldap_schemas.account.objects.get(uid=ua.username)
+        p.rename(uid=new_username)

karaage/applications/emails.py
@@ -87,12 +87,14 @@ def send_user_invite_email(userapplication):
     send_mail(subject, body, settings.ACCOUNTS_EMAIL, [to_email], fail_silently=False)
 
 
-def send_account_approved_email(userapplication):
+def send_account_approved_email(userapplication, created_person):
     """Sends an email informing person account is ready"""
     context = CONTEXT.copy()
     context['receiver'] = userapplication.applicant
     context['project'] = userapplication.project
     context['site'] = '%s/profile/' % settings.REGISTRATION_BASE_URL
+    if created_person:
+        context['reset_url'] = userapplication.applicant.get_password_reset_url()
     subject, body = render_email('account_approved', context)
     to_email = userapplication.applicant.email
     
@@ -125,14 +127,22 @@ def send_project_request_email(application):
         send_mail(subject, body, settings.ACCOUNTS_EMAIL, [to_email], fail_silently=False)
 
 
-def send_project_approved_email(application):
+def send_project_approved_email(application, created_person):
     """Sends an email to the projects leaders once approved"""
     context = CONTEXT.copy()
     context['site'] = '%s/projects/%s/' % (settings.REGISTRATION_BASE_URL, application.project.pid)
     context['project'] = application.project
  
+    context['receiver'] = application.applicant
+    if created_person:
+        context['reset_url'] = application.applicant.get_password_reset_url()
+    subject, body = render_email('project_approved', context)
+    to_email = application.applicant.email
+    send_mail(subject, body, settings.ACCOUNTS_EMAIL, [to_email], fail_silently=False)
+
     for leader in application.project.leaders.all():
-        context['receiver'] = leader
-        subject, body = render_email('project_approved', context)
-        to_email = leader.email
-        send_mail(subject, body, settings.ACCOUNTS_EMAIL, [to_email], fail_silently=False)
+        if leader != application.applicant:
+            context['receiver'] = leader
+            subject, body = render_email('project_approved', context)
+            to_email = leader.email
+            send_mail(subject, body, settings.ACCOUNTS_EMAIL, [to_email], fail_silently=False)

karaage/applications/forms.py
@@ -21,14 +21,12 @@ from django import forms
 from django.conf import settings
 from django.db.models import Q
 
-from andsome.util import is_password_strong
 from captcha.fields import CaptchaField
 
 from karaage.applications.models import UserApplication, ProjectApplication, Applicant
 from karaage.people.models import Person, Institute
 from karaage.people.utils import validate_username, UsernameException
 from karaage.projects.models import Project
-from karaage.datastores import create_password_hash
 
 APP_CHOICES = (
     ('U', 'Join an existing project'),
@@ -108,23 +106,8 @@ class UserApplicantForm(ApplicantForm):
         self.fields['institute'].required = True
         self.fields['telephone'].required = True
 
-    password1 = forms.CharField(widget=forms.PasswordInput(render_value=False), label=u'Password')
-    password2 = forms.CharField(widget=forms.PasswordInput(render_value=False), label=u'Password (again)')
     institute = forms.ModelChoiceField(queryset=Institute.active.filter(Q(saml_entityid="") | Q(saml_entityid__isnull=True)))
     
-    def clean_password2(self):
-        data = self.cleaned_data
-
-        if data.get('password1') and data.get('password2'):
-        
-            if data['password1'] != data['password2']:
-                raise forms.ValidationError(u'You must type the same password each time')
-
-            if not is_password_strong(data['password1']):
-                raise forms.ValidationError(u'Your password was found to be insecure, a good password has a combination of letters (uppercase, lowercase), numbers and is at least 8 characters long.')
-
-            return data
-
     def clean_email(self):
         email = self.cleaned_data['email']
         users = Person.active.filter(user__email__exact=email)
@@ -135,7 +118,6 @@ class UserApplicantForm(ApplicantForm):
 
     def save(self, commit=True):
         applicant = super(UserApplicantForm, self).save(commit=commit)
-        applicant.password = create_password_hash(self.cleaned_data['password1'])
         if commit:
             applicant.save()
         return applicant

karaage/applications/migrations/0014_auto__del_field_applicant_password.py
@@ -1,0 +1,188 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'Applicant.password'
+        db.delete_column(u'applications_applicant', 'password')
+
+
+    def backwards(self, orm):
+        # Adding field 'Applicant.password'
+        db.add_column(u'applications_applicant', 'password',
+                      self.gf('django.db.models.fields.CharField')(max_length=128, null=True, blank=True),
+                      keep_default=False)
+
+
+    models = {
+        u'applications.applicant': {
+            'Meta': {'object_name': 'Applicant'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'country': ('django.db.models.fields.CharField', [], {'max_length': '2', 'null': 'True', 'blank': 'True'}),
+            'department': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
+            'email_verified': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'fax': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'institute': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['people.Institute']", 'null': 'True', 'blank': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
+            'mobile': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'position': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'postcode': ('django.db.models.fields.CharField', [], {'max_length': '8', 'null': 'True', 'blank': 'True'}),
+            'saml_id': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'supervisor': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'telephone': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '16', 'unique': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        u'applications.application': {
+            'Meta': {'object_name': 'Application'},
+            '_class': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'complete_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['people.Person']", 'null': 'True', 'blank': 'True'}),
+            'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'expires': ('django.db.models.fields.DateTimeField', [], {}),
+            'header_message': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'secret_token': ('django.db.models.fields.CharField', [], {'default': "'dd759b605936ec7c1f7c9bf4f2a861efd35ae013'", 'unique': 'True', 'max_length': '64'}),
+            'state': ('django.db.models.fields.CharField', [], {'default': "'N'", 'max_length': '1'}),
+            'submitted_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'applications.projectapplication': {
+            'Meta': {'object_name': 'ProjectApplication', '_ormbases': [u'applications.Application']},
+            'additional_req': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'application_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['applications.Application']", 'unique': 'True', 'primary_key': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'institute': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['people.Institute']"}),
+            'machine_categories': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['machines.MachineCategory']", 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'needs_account': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Project']", 'null': 'True', 'blank': 'True'})
+        },
+        u'applications.userapplication': {
+            'Meta': {'object_name': 'UserApplication', '_ormbases': [u'applications.Application']},
+            u'application_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['applications.Application']", 'unique': 'True', 'primary_key': 'True'}),
+            'make_leader': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'needs_account': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Project']", 'null': 'True', 'blank': 'True'})
+        },
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'machines.machinecategory': {
+            'Meta': {'object_name': 'MachineCategory', 'db_table': "'machine_category'"},
+            'datastore': ('django.db.models.fields.CharField', [], {'default': "'karaage.datastores.openldap_datastore'", 'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'people.institute': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Institute', 'db_table': "'institute'"},
+            'delegates': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'delegate'", 'to': u"orm['people.Person']", 'through': u"orm['people.InstituteDelegate']", 'blank': 'True', 'symmetrical': 'False', 'null': 'True'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'saml_entityid': ('django.db.models.fields.CharField', [], {'max_length': '200', 'unique': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        u'people.institutedelegate': {
+            'Meta': {'object_name': 'InstituteDelegate'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'institute': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['people.Institute']"}),
+            'person': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['people.Person']"}),
+            'send_email': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        u'people.person': {
+            'Meta': {'ordering': "['user__first_name', 'user__last_name']", 'object_name': 'Person', 'db_table': "'person'"},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'approved_by': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'user_approver'", 'null': 'True', 'to': u"orm['people.Person']"}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'comment': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'country': ('django.db.models.fields.CharField', [], {'max_length': '2', 'null': 'True', 'blank': 'True'}),
+            'date_approved': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'date_deleted': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'deleted_by': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'user_deletor'", 'null': 'True', 'to': u"orm['people.Person']"}),
+            'department': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'expires': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'fax': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'institute': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['people.Institute']"}),
+            'is_systemuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_usage': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'login_enabled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'mobile': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'position': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'postcode': ('django.db.models.fields.CharField', [], {'max_length': '8', 'null': 'True', 'blank': 'True'}),
+            'saml_id': ('django.db.models.fields.CharField', [], {'max_length': '200', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': '4', 'null': 'True', 'blank': 'True'}),
+            'supervisor': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'telephone': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'unique': 'True'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        u'projects.project': {
+            'Meta': {'ordering': "['pid']", 'object_name': 'Project', 'db_table': "'project'"},
+            'additional_req': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'approved_by': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'project_approver'", 'null': 'True', 'to': u"orm['people.Person']"}),
+            'date_approved': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'date_deleted': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'deleted_by': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'project_deletor'", 'null': 'True', 'to': u"orm['people.Person']"}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'end_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'institute': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['people.Institute']"}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_approved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_usage': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'leaders': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'leaders'", 'symmetrical': 'False', 'to': u"orm['people.Person']"}),
+            'machine_categories': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'projects'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['machines.MachineCategory']"}),
+            'machine_category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['machines.MachineCategory']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'pid': ('django.db.models.fields.CharField', [], {'max_length': '50', 'primary_key': 'True'}),
+            'start_date': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2013, 6, 7, 0, 0)'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['people.Person']", 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['applications']

commit 4a896cf9f878845812b96840e897a2a07ab76a54

Don't set initial password.
Setting the initial password on new accounts to '' was broken behaviour
introduced by 96fc5ae69bc0d1c0037e2223f0dccfcf6347abed.

Don't set userPassword field, this should result in a account
with no usable password.

Change-Id: I98bfaeebf39789e2a45dd177086fef7b8ecb78f4

karaage/datastores/ldap_datastore.py
@@ -45,7 +45,6 @@ class AccountDataStore(base.AccountDataStore):
         luser.mail = str_or_none(person.email)
         luser.title = str_or_none(person.title)
         luser.o = person.institute.name
-        luser.userPassword = "" # FIXME
 
         luser.gidNumber = person.institute.gid
         luser.homeDirectory = settings.HOME_DIRECTORY % { 'default_project': ua.default_project.pid, 'uid': luser.uid }