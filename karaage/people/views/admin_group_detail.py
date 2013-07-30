38cf070d82e55254cf2e09f90a464b6399b23617
Brian May <brian@microcomaustralia.com.au>
2013-07-30 16:21:13 +1000

    Fix delete group
    * Don't allow delete group if other objects depend on it.
    * Fix the redirect url on completion.
    * Fix the breadcrumbs.
    
    Change-Id: I891b52cbe8900e781dfb973c996dc577ae07befd

--- karaage/people/views/admin_group_detail.py
+++ karaage/people/views/admin_group_detail.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Karaage  If not, see <http://www.gnu.org/licenses/>.
 
+from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
@@ -30,10 +31,17 @@ def delete_group(request, group_name):
 
     group = get_object_or_404(Group, name=group_name)
 
-    if request.method == 'POST':
+    error = None
+    if group.softwarepackage_set.all().count() > 0:
+        error = "A software package is using this group."
+    elif group.institute_set.all().count() > 0:
+        error = "An institute is using this group."
+    elif group.project_set.all().count() > 0:
+        error = "A project is using this group."
+    elif request.method == 'POST':
         group.delete()
         messages.success(request, "Group '%s' was deleted succesfully" % group)
-        return HttpResponseRedirect(group.get_absolute_url())
+        return HttpResponseRedirect(reverse("kg_group_list"))
 
     return render_to_response('people/group_confirm_delete.html', locals(), context_instance=RequestContext(request))
 
