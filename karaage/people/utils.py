9c1b7288c8c061135d3515d3bc8830670bff9201
Brian May <brian@microcomaustralia.com.au>
2013-09-03 11:29:46 +1000

    Remove unneeded file.
    Change-Id: Ib5a1adc3b1068c8a16ba7613b9362ad185af3b83

--- karaage/people/utils.py
+++ karaage/people/utils.py
@@ -1,10 +1,29 @@
+# Copyright 2007-2013 VPAC
+#
+# This file is part of Karaage.
+#
+# Karaage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Karaage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Karaage  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+
 from django.contrib.auth.models import User
 from django.conf import settings
 
 from karaage.datastores import person_exists, account_exists
-from karaage.validators import username_re
 from karaage.machines.models import MachineCategory
 
+username_re = re.compile(r'^[-\w]+$')
 
 class UsernameException(Exception):
     pass
