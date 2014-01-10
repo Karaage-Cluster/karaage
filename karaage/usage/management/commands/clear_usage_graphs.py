1187d291eab5fd1457b4e209aef0303db7644c8d
Russell Sim <russell.sim@gmail.com>
2014-01-10 14:52:37 +1100

    Handle graph directories not existing.
    - Added handling for when the directory containing the graphs
      doesn't exist.
    - Removed duplicate code from the clear_usage_graphs management script.

--- karaage/usage/management/commands/clear_usage_graphs.py
+++ karaage/usage/management/commands/clear_usage_graphs.py
@@ -23,45 +23,28 @@ GRAPH_BASE_DIR = settings.GRAPH_ROOT
 
 class Command(BaseCommand):
     help = "Deletes all usage graphs"
-    
+    verbose = 0
+
+    def _clean_resource(self, resource_type):
+
+        if self.verbose > 1:
+            print "Cleaning all %s graphs." % (resource_type.title())
+
+        RESOURCE_DIR = os.path.join(GRAPH_BASE_DIR, resource_type, '')
+        if not os.path.exists(RESOURCE_DIR):
+            print "No %s graphs to clean." % (resource_type.title())
+            return
+
+        file_list = os.listdir(RESOURCE_DIR)
+        for f in file_list:
+            if not f == '.svn':
+                os.remove('%s%s' % (RESOURCE_DIR,f))
+
+
     def handle(self, **options):
-        verbose = int(options.get('verbosity'))
+        self.verbose = int(options.get('verbosity'))
 
-        if verbose > 1:
-            print "Cleaning all Project graphs"
-
-        PROJECT_DIR = '%s/projects/' % GRAPH_BASE_DIR
-        file_list = os.listdir(PROJECT_DIR)
-        for f in file_list:
-            if not f == '.svn':
-                os.remove('%s%s' % (PROJECT_DIR,f))
-            
-        if verbose > 1:
-            print "Cleaning all Institute graphs"
-
-        INSTITUTE_DIR = '%s/institutes/' % GRAPH_BASE_DIR
-        file_list = os.listdir(INSTITUTE_DIR)
-        for f in file_list:
-            if not f == '.svn':
-                os.remove('%s%s' % (INSTITUTE_DIR,f))
-
-        if verbose > 1:
-            print "Cleaning all trend graphs"
-        
-
-        TRENDS_DIR = '%s/trends/' % GRAPH_BASE_DIR
-        file_list = os.listdir(TRENDS_DIR)
-        for f in file_list:
-            if not f == '.svn':
-                os.remove('%s%s' % (TRENDS_DIR,f))
-
-        if verbose > 1:
-            print "Cleaning all institute trend graphs"
-
-        I_TRENDS_DIR = '%s/i_trends/' % GRAPH_BASE_DIR
-        file_list = os.listdir(I_TRENDS_DIR)
-        for f in file_list:
-            if not f == '.svn':
-                os.remove('%s%s' % (I_TRENDS_DIR,f))
-
-
+        self._clean_resource('projects')
+        self._clean_resource('institutes')
+        self._clean_resource('trends')
+        self._clean_resource('i_trends')
