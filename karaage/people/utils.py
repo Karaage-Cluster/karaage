257cb2dde97a517e3ddd4a6880765a91545a4751
Russell Sim <russell.sim@gmail.com>
2014-01-29 14:06:56 +1100

    Fixed bad indentation
    Change-Id: I4ba70bfb01638f8440eed304502ba11eb5bb60e6

--- karaage/people/utils.py
+++ karaage/people/utils.py
@@ -35,31 +35,30 @@ class UsernameInvalid(UsernameException):
 
 class UsernameTaken(UsernameException):
     pass
-    
-    
+
+
 def validate_username(username):
-    
-        if username:
-            if not username.islower():
-                raise UsernameInvalid(u'Username must be all lowercase')
-            if len(username) < 2:
-                raise UsernameInvalid(u'Username must be at least 2 characters')
-            if not username_re.search(username):
-                raise UsernameInvalid(u'Usernames can only contain letters, numbers and underscores')
+    if username:
+        if not username.islower():
+            raise UsernameInvalid(u'Username must be all lowercase')
+        if len(username) < 2:
+            raise UsernameInvalid(u'Username must be at least 2 characters')
+        if not username_re.search(username):
+            raise UsernameInvalid(u'Usernames can only contain letters, numbers and underscores')
 
-            try:
-                user = Person.objects.get(username__exact=username)
-            except Person.DoesNotExist:
-                user = None
+        try:
+            user = Person.objects.get(username__exact=username)
+        except Person.DoesNotExist:
+            user = None
 
-            if user is not None:
-                raise UsernameTaken(u'The username is already taken. Please choose another. If this was the name of your old account please email %s' % settings.ACCOUNTS_EMAIL)
+        if user is not None:
+            raise UsernameTaken(u'The username is already taken. Please choose another. If this was the name of your old account please email %s' % settings.ACCOUNTS_EMAIL)
 
-            if person_exists(username):
-                raise UsernameTaken(u'Username is already in external personal datastore.')
+        if person_exists(username):
+            raise UsernameTaken(u'Username is already in external personal datastore.')
 
-            for mc in MachineCategory.objects.all():
-                if account_exists(username, mc):
-                     raise UsernameTaken(u'Username is already in external account datastore.')
+        for mc in MachineCategory.objects.all():
+            if account_exists(username, mc):
+                 raise UsernameTaken(u'Username is already in external account datastore.')
 
-        return username
+    return username
