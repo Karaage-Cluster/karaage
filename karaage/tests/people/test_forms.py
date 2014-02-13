04067d2c7a8566fa3ae924ec782d3046313631e2
Russell Sim <russell.sim@gmail.com>
2014-02-13 17:51:42 +1100

    Increase the max username length to 255
    I expect that the backend you select should determine the max length ofyes
    a username.  This should probably be enforced at the form level, and the
    max length should be made configurable.
    
    Change-Id: Ief55c664bd2565b6db3f358690d2dd27df332e72

---
+++ karaage/tests/people/test_forms.py
@@ -0,0 +1,75 @@
+# Copyright 2014 The University of Melbourne
+#
+# This file is part of Karaage.
+#
+# Karaage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Karaage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Karaage  If not, see <http://www.gnu.org/licenses/>.
+
+from django.core import exceptions as django_exceptions
+from django.test import TestCase
+
+from karaage.institutes.models import Institute
+from karaage.people.forms import AddPersonForm
+from karaage.tests.fixtures import ProjectFactory
+
+
+class AddPersonFormTestCase(TestCase):
+    def _valid_user(self):
+        project = ProjectFactory()
+        valid_user = {
+            'username': 'testuser',
+            'project': project.id,
+            'institute': project.institute.id,
+            'full_name': 'Joe Blow',
+            'short_name': 'Joe',
+            'needs_account': True,
+            'email': 'test@example.com',
+            'telephone': '8888888888',
+            'password1': 'test2134uuutuut',
+            'password2': 'test2134uuutuut'}
+        return valid_user
+
+    def test_valid_data(self):
+        form_data = self._valid_user()
+        form = AddPersonForm(data=form_data)
+        self.assertEqual(form.is_valid(), True, form.errors.items())
+
+    def test_invalid_username(self):
+        form_data = self._valid_user()
+        form_data['username'] = '!invalid!'
+        form = AddPersonForm(data=form_data)
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(
+            form.errors.items(),
+            [('username',
+              [u'Usernames can only contain letters, numbers and underscores'])])
+
+    def test_upper_username(self):
+        form_data = self._valid_user()
+        form_data['username'] = 'INVALID'
+        form = AddPersonForm(data=form_data)
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(
+            form.errors.items(),
+            [('username',
+              [u'Username must be all lowercase'])])
+
+    def test_long_username(self):
+        form_data = self._valid_user()
+        form_data['username'] = 'long' * 100
+        form = AddPersonForm(data=form_data)
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(
+            form.errors.items(),
+            [('username',
+              [u'Ensure this value has at most 255 characters (it has 400).'])])
