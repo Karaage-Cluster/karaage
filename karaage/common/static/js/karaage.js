229697883e05a3bcca73be34141277be989ace5d
Brian May <brian@microcomaustralia.com.au>
2014-02-18 16:34:15 +1100

    WIP - move action buttons.
    Try to make it easier to find the action buttons.
    
    Change-Id: I76d91ee9ebd64edad50f13d5ea6591ba0ec317b8

--- karaage/common/static/js/karaage.js
+++ karaage/common/static/js/karaage.js
@@ -3,3 +3,15 @@ function showHidden() {
   $("#hidden-link").hide("fast");
   $("#hidden").slideDown("slow");
 }
+
+$( document ).ready(function() {
+    $('.object-tools li').each(function() {
+        $(this).click(function(ev) {
+            ev.preventDefault();
+            var a = $(this).find("a");
+            if (a.length > 0) {
+                window.location.href = a.attr('href');
+            }
+        });
+    });
+});
