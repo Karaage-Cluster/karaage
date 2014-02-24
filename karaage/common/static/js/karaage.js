82e087bfc14a67acf3808d43079f4081c8456841
Brian May <brian@microcomaustralia.com.au>
2014-02-24 13:55:34 +1100

    Update style issues.
    Change-Id: If89ce9a6bb0e0f26bb58d50fe6b8163e8f178b09

--- karaage/common/static/js/karaage.js
+++ karaage/common/static/js/karaage.js
@@ -1,5 +1,19 @@
-
-function showHidden() {
-  $("#hidden-link").hide("fast");
-  $("#hidden").slideDown("slow");
-}
+$( document ).ready(function() {
+    $.each($(".hidden-link"), function (index, element) {
+        $(this).find("a").on("click", function(ev) {
+            var hl = $(this).parent().parent()
+            var next = hl.next()
+            while (next.length > 0) {
+                next.show("slow");
+                next = next.next();
+            }
+            hl.remove();
+            ev.preventDefault();
+        })
+        var next = $(this).next()
+        while (next.length > 0) {
+            next.hide();
+            next = next.next();
+        }
+    })
+})
