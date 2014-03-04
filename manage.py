eb30ed726b8a22d64ab149928f36af0f5a2bf036
Russell Sim <russell.sim@gmail.com>
2014-03-05 10:32:48 +1100

    Move tests into karaage module namespace
    Change-Id: Ie87021804f850220e30ccc426abd6042ada9a8c8

--- manage.py
+++ manage.py
@@ -2,6 +2,6 @@
 import os
 from django.core import management
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'karaage.conf.settings'
 if __name__ == "__main__":
     management.execute_from_command_line()
