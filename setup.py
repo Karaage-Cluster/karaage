b31082de540fec44741542b6f81a33a0b58d28a3
Brian May <brian@microcomaustralia.com.au>
2014-03-07 09:29:31 +1100

    Replace distutils with setuptools.
    Change-Id: Iba20428fe37282f24953b0bff608aa85d4f26ea3

--- setup.py
+++ setup.py
@@ -17,10 +17,11 @@
 # You should have received a copy of the GNU General Public License
 # along with django-tldap  If not, see <http://www.gnu.org/licenses/>.
 
-from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
+from setuptools import setup, find_packages
 import os
-import sys
+
+with open('VERSION.txt', 'r') as f:
+    version = f.readline().strip()
 
 def fullsplit(path, result=None):
     """
@@ -36,50 +37,43 @@ def fullsplit(path, result=None):
         return result
     return fullsplit(head, [tail] + result)
 
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-code_dir = 'karaage'
-
-for dirpath, dirnames, filenames in os.walk(code_dir):
+packages = []
+for dirpath, dirnames, filenames in os.walk("karaage"):
     # Ignore dirnames that start with '.'
     for i, dirname in enumerate(dirnames):
         if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-with open('VERSION.txt', 'r') as f:
-    version = f.readline().strip()
-
-for dirpath, dirnames, filenames in os.walk('conf'):
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
     if filenames:
-        data_files.append([dirpath.replace('conf', '/etc/karaage'), [os.path.join(dirpath, f) for f in filenames]])
-
-
-data_files.append(
-    ('/usr/sbin', ['sbin/kg_set_secret_key'])
-)
+        packages.append('.'.join(fullsplit(dirpath)))
 
 setup(
     name = "karaage",
     version = version,
-    url = 'http://code.vpac.org/trac/karaage/',
+    url = 'https://github.com/Karaage-Cluster/karaage',
     author = 'Brian May',
     author_email = 'brian@vpac.org',
     description = 'Collection of Django apps to manage a clusters',
+    licence = "GPL3+",
     packages = packages,
-    data_files = data_files,
+    package_data = {
+        '': [ '*.css', '*.html', '*.js', '*.png', '*.gif', '*.map', '*.txt' ],
+    },
+    scripts = [ 'sbin/kg_set_secret_key' ],
+    data_files=[
+        ('/etc/karaage', [ 'conf/global_settings.py', ]),
+    ],
+    install_requires = [
+        "python > 2.4",
+        "Django >= 1.6",
+        "South >= 0.7",
+        "python-alogger >= 2.0",
+        "django-xmlrpc >= 0.1",
+        "django-simple-captcha",
+        "django-ajax-selects >= 1.1.3",
+        "django_jsonfield >= 0.9.12",
+        "django-model-utils >= 2.0.0",
+        "django-tldap >= 0.2.9",
+        "django-xmlrpc >= 0.1",
+        "django_celery",
+        "django_model_utils >= 2.0.0",
+    ],
 )
