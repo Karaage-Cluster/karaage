54e665265d8369642d3e8f1608be4c032fcc661e
Brian May <brian@microcomaustralia.com.au>
2014-03-06 15:55:41 +1100

    Add hash bang and comment.
    Change-Id: I0dcf2885e525cd2849b992280c546a2ebce0db03

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python
+
+# Copyright 2012-2014 VPAC
+#
+# This file is part of django-tldap.
+#
+# django-tldap is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# django-tldap is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with django-tldap  If not, see <http://www.gnu.org/licenses/>.
+
 from distutils.core import setup
 from distutils.command.install_data import install_data
 from distutils.command.install import INSTALL_SCHEMES
