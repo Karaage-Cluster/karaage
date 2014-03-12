8d0d1d40befc8f1f4b6595daddb2548f22bbb2dc
Brian May <brian@microcomaustralia.com.au>
2014-03-13 09:31:33 +1100

    Add run_tests.sh script.
    For consistancy with Karaage 3.
    
    Change-Id: I65d831bb96fce8214fd2e522b696b8a5e0cf179d

---
+++ run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+RETURN=0
+cd $DIR
+
+if [ -n "$*" ]; then
+    TESTS="$@"
+else
+    TESTS="karaage"
+fi
+
+# NOTE (RS) Disabled because there are far too many errors to fix.
+
+# flake8 .
+# if [ ! $? -eq 0 ]
+# then
+#     RETURN=1
+# fi
+
+
+cd tests
+./manage.py test -v 2 $TESTS
+if [ ! $? -eq 0 ]
+then
+    RETURN=1
+fi
+
+exit $RETURN
