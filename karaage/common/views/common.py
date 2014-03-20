Karaage-Cluster/karaage

commit bad98d8d71441121cec62a5c0d5b1fe1eca0dc5e

Fix PEP8 issues.
Change-Id: Ic5fc5c0853431871e5027e1453cf40e24f66171d

karaage/common/views/common.py
@@ -33,18 +33,31 @@ from karaage.projects.models import Project
 
 @admin_required
 def admin_index(request):
+    newest_users = Person.objects.order_by('-date_approved', '-id')
+    newest_users = newest_users.filter(date_approved__isnull=False)
+    newest_users = newest_users.select_related()[:5],
+
+    newest_projects = Project.objects.order_by('-date_approved')
+    newest_projects = newest_projects.filter(date_approved__isnull=False)
+    newest_projects = newest_projects.filter(is_active=True)
+    newest_projects = newest_projects.select_related()[:5],
+
+    recent_actions = request.user.logentry_set.all()[:10],
+
     var = {
-        'newest_users': Person.objects.order_by('-date_approved', '-id').filter(date_approved__isnull=False).select_related()[:5],
-        'newest_projects': Project.objects.order_by('-date_approved').filter(date_approved__isnull=False).filter(is_active=True).select_related()[:5],
-        'recent_actions': request.user.logentry_set.all()[:10],
+        'newest_users': newest_users,
+        'newest_projects': newest_projects,
+        'recent_actions': recent_actions,
     }
-    return render_to_response('common/index.html', var, context_instance=RequestContext(request))
+    return render_to_response(
+        'common/index.html', var, context_instance=RequestContext(request))
+
 
 def index(request):
     if settings.ADMIN_REQUIRED or is_admin(request):
         return admin_index(request)
-    return render_to_response('common/index.html', context_instance=RequestContext(request))
-
+    return render_to_response(
+        'common/index.html', context_instance=RequestContext(request))
 
 
 @admin_required
@@ -62,7 +75,10 @@ def search(request):
         # users
         query = Q()
         for term in term_list:
-            q = Q(username__icontains=term) | Q(short_name__icontains=term) | Q(full_name__icontains=term) | Q(email__icontains=term)
+            q = Q(username__icontains=term)
+            q = q | Q(short_name__icontains=term)
+            q = q | Q(full_name__icontains=term)
+            q = q | Q(email__icontains=term)
             query = query & q
 
         people_list = people_list.filter(query).distinct()
@@ -78,7 +94,11 @@ def search(request):
          # projects
         query = Q()
         for term in term_list:
-            q = Q(pid__icontains=term) | Q(name__icontains=term) | Q(leaders__username__icontains=term) | Q(leaders__short_name__icontains=term) | Q(leaders__full_name__icontains=term)
+            q = Q(pid__icontains=term)
+            q = q | Q(name__icontains=term)
+            q = q | Q(leaders__username__icontains=term)
+            q = q | Q(leaders__short_name__icontains=term)
+            q = q | Q(leaders__full_name__icontains=term)
             query = query & q
 
         project_list = project_list.filter(query).distinct()
@@ -87,8 +107,11 @@ def search(request):
 
         if not (people_list or group_list or project_list):
             empty = True
-        
-        return render_to_response('common/site_search.html', locals(), context_instance=RequestContext(request))
+
+        return render_to_response(
+            'common/site_search.html',
+            locals(),
+            context_instance=RequestContext(request))
     else:
         return HttpResponseRedirect(reverse('index'))
 
@@ -101,7 +124,9 @@ def log_list(request):
         terms = request.REQUEST['search'].lower()
         query = Q()
         for term in terms.split(' '):
-            q = Q(user__username__iexact=term) | Q(object_repr__iexact=term) | Q(change_message__icontains=term)
+            q = Q(user__username__iexact=term)
+            q = q | Q(object_repr__iexact=term)
+            q = q | Q(change_message__icontains=term)
             query = query & q
 
         log_list = log_list.filter(query)
@@ -121,18 +146,21 @@ def log_list(request):
         page = paginator.page(paginator.num_pages)
 
     return render_to_response(
-                'common/log_list.html',
-                {'page': page, 'short': False, 'terms': terms},
-                context_instance=RequestContext(request))
+        'common/log_list.html',
+        {'page': page, 'short': False, 'terms': terms},
+        context_instance=RequestContext(request))
 
 
 @admin_required
 def misc(request):
     from karaage.common.simple import direct_to_template
-    return direct_to_template(request,
-            template='common/misc_detail.html')
+    return direct_to_template(
+        request,
+        template='common/misc_detail.html')
+
 
 def aup(request):
     from karaage.common.simple import direct_to_template
-    return direct_to_template(request,
-            template='common/aup_detail.html')
+    return direct_to_template(
+        request,
+        template='common/aup_detail.html')

karaage/applications/views/common.py
@@ -29,18 +29,16 @@ from karaage.common.decorators import admin_required, login_required
 from karaage.applications.models import Applicant, Application
 from karaage.applications.forms import ApplicantForm
 import karaage.applications.views.base as base
-from karaage.common import log
 import karaage.common as util
+
 
 @login_required
 def application_list(request):
     """ a logged in user wants to see all his pending applications. """
-    person = request.user
     applications = Application.objects.all()
 
     if not util.is_admin(request):
         applications = Application.objects.get_for_applicant(request.user)
-
 
     if 'search' in request.REQUEST:
         terms = request.REQUEST['search'].lower()
@@ -73,6 +71,7 @@ def application_list(request):
         {'page': page, 'terms': terms},
         context_instance=RequestContext(request))
 
+
 @login_required
 def profile_application_list(request):
     """ a logged in user wants to see all his pending applications. """
@@ -81,13 +80,13 @@ def profile_application_list(request):
     requires_attention = Application.objects.requires_attention(request)
 
     return render_to_response(
-            'applications/profile_applications.html',
-            {
+        'applications/profile_applications.html',
+        {
             'person': request.user,
             'my_applications': my_applications,
             'requires_attention': requires_attention,
-            },
-            context_instance=RequestContext(request))
+        },
+        context_instance=RequestContext(request))
 
 
 @admin_required
@@ -101,16 +100,19 @@ def applicant_edit(request, applicant_id):
             messages.success(request, "%s modified successfully." % applicant)
             return HttpResponseRedirect(reverse('kg_application_list'))
 
-    return render_to_response('applications/applicant_form.html',
-            {'form': form}, context_instance=RequestContext(request))
+    return render_to_response(
+        'applications/applicant_form.html',
+        {'form': form}, context_instance=RequestContext(request))
 
 
 @admin_required
 def application_logs(request, application_id):
     obj = get_object_or_404(Application, pk=application_id)
     breadcrumbs = []
-    breadcrumbs.append( ("Applications", reverse("kg_application_list")) )
-    breadcrumbs.append( (unicode(obj), reverse("kg_application_detail", args=[obj.pk])) )
+    breadcrumbs.append(
+        ("Applications", reverse("kg_application_list")))
+    breadcrumbs.append(
+        (unicode(obj), reverse("kg_application_detail", args=[obj.pk])))
     return util.log_list(request, breadcrumbs, obj)
 
 
@@ -118,8 +120,10 @@ def application_logs(request, application_id):
 def add_comment(request, application_id):
     obj = get_object_or_404(Application, pk=application_id)
     breadcrumbs = []
-    breadcrumbs.append( ("Applications", reverse("kg_application_list")) )
-    breadcrumbs.append( (unicode(obj), reverse("kg_application_detail", args=[obj.pk])) )
+    breadcrumbs.append(
+        ("Applications", reverse("kg_application_list")))
+    breadcrumbs.append(
+        (unicode(obj), reverse("kg_application_detail", args=[obj.pk])))
     return util.add_comment(request, breadcrumbs, obj)
 
 
@@ -130,18 +134,20 @@ def application_detail(request, application_id, state=None, label=None):
     state_machine = base.get_state_machine(application)
     return state_machine.process(request, application, state, label)
 
+
 def application_unauthenticated(request, token, state=None, label=None):
     """ An somebody is trying to access an application. """
     application = base.get_application(
-                secret_token=token, expires__gt=datetime.datetime.now())
+        secret_token=token, expires__gt=datetime.datetime.now())
 
     # redirect user to real url if possible.
     if request.user.is_authenticated():
         if request.user == application.applicant:
-            url = base.get_url(request, application, {'is_applicant': True}, label)
+            url = base.get_url(
+                request, application, {'is_applicant': True}, label)
             return HttpResponseRedirect(url)
 
     state_machine = base.get_state_machine(application)
-    return state_machine.process(request, application, state, label,
-            { 'is_applicant': True })
-
+    return state_machine.process(
+        request, application, state, label,
+        {'is_applicant': True})

karaage/common/__init__.py
@@ -37,7 +37,12 @@ from django.template import add_to_builtins
 add_to_builtins('karaage.common.templatetags.karaage_tags')
 
 
-def get_date_range(request, default_start=(datetime.date.today() - datetime.timedelta(days=90)), default_end=datetime.date.today()):
+def get_date_range(request, default_start=None, default_end=None):
+
+    if default_start is None:
+        default_start = datetime.date.today() - datetime.timedelta(days=90)
+    if default_end is None:
+        default_end = datetime.date.today()
 
     today = datetime.date.today()
 
@@ -106,14 +111,15 @@ class log():
 def new_random_token():
     import random
     from hashlib import sha1
-    from django.conf import settings
     # Use the system (hardware-based) random number generator if it exists.
     if hasattr(random, 'SystemRandom'):
         randrange = random.SystemRandom().randrange
     else:
         randrange = random.randrange
     MAX_KEY = 18446744073709551616L     # 2 << 63
-    return sha1("%s%s" % (randrange(0, MAX_KEY), settings.SECRET_KEY)).hexdigest()
+
+    string = "%s%s" % (randrange(0, MAX_KEY), settings.SECRET_KEY)
+    return sha1(string).hexdigest()
 
 
 def log_list(request, breadcrumbs, obj):
@@ -126,7 +132,9 @@ def log_list(request, breadcrumbs, obj):
         terms = request.REQUEST['search'].lower()
         query = Q()
         for term in terms.split(' '):
-            q = Q(user__username__iexact=term) | Q(object_repr__iexact=term) | Q(change_message__icontains=term)
+            q = Q(user__username__iexact=term)
+            q = q | Q(object_repr__iexact=term)
+            q = q | Q(change_message__icontains=term)
             query = query & q
 
         log_list = log_list.filter(query)
@@ -146,13 +154,14 @@ def log_list(request, breadcrumbs, obj):
         page = paginator.page(paginator.num_pages)
 
     return render_to_response(
-            'common/log_list.html',
-            { 'short': True, 'obj': obj,
-                'page': page,
-                'breadcrumbs': breadcrumbs,
-                'terms': terms,
-            },
-            context_instance=RequestContext(request))
+        'common/log_list.html',
+        {
+            'short': True, 'obj': obj,
+            'page': page,
+            'breadcrumbs': breadcrumbs,
+            'terms': terms,
+        },
+        context_instance=RequestContext(request))
 
 
 def add_comment(request, breadcrumbs, obj):
@@ -164,11 +173,12 @@ def add_comment(request, breadcrumbs, obj):
         return HttpResponseRedirect(obj.get_absolute_url())
 
     return render_to_response(
-            'common/add_comment.html',
-            { 'form': form, 'obj': obj,
-                'breadcrumbs': breadcrumbs,
-            },
-            context_instance=RequestContext(request))
+        'common/add_comment.html',
+        {
+            'form': form, 'obj': obj,
+            'breadcrumbs': breadcrumbs,
+        },
+        context_instance=RequestContext(request))
 
 
 def is_admin(request):
@@ -177,6 +187,7 @@ def is_admin(request):
     if not request.user.is_authenticated():
         return False
     return request.user.is_admin
+
 
 def get_hooks(name):
     for app in settings.INSTALLED_APPS:
@@ -189,6 +200,7 @@ def get_hooks(name):
             function = getattr(module, name, None)
             if function is not None:
                 yield function
+
 
 def get_urls(name):
     for app in settings.INSTALLED_APPS:

karaage/common/constants.py
@@ -31,13 +31,26 @@ TITLES = (
 )
 
 DATE_FORMATS = [
+    # '2006-10-25'
     '%Y-%m-%d',
-    '%d-%m-%y', '%d/%m/%y', '%d/%m/%y',  # '25-10-06', '25/10/06', '25/10/06'
-    '%d-%m-%Y', '%d/%m/%Y', '%d/%m/%Y',  # '25-10-2006', '25/10/2006', '25/10/2006'
-    '%b %d %Y', '%b %d, %Y',             # 'Oct 25 2006', 'Oct 25, 2006'
-    '%d %b %Y', '%d %b, %Y',             # '25 Oct 2006', '25 Oct, 2006'
-    '%B %d %Y', '%B %d, %Y',             # 'October 25 2006', 'October 25, 2006'
-    '%d %B %Y', '%d %B, %Y',             # '25 October 2006', '25 October, 2006'
+
+    # '25-10-06', '25/10/06', '25/10/06'
+    '%d-%m-%y', '%d/%m/%y', '%d/%m/%y',
+
+    # '25-10-2006', '25/10/2006', '25/10/2006'
+    '%d-%m-%Y', '%d/%m/%Y', '%d/%m/%Y',
+
+    # 'Oct 25 2006', 'Oct 25, 2006'
+    '%b %d %Y', '%b %d, %Y',
+
+    # '25 Oct 2006', '25 Oct, 2006
+    '%d %b %Y', '%d %b, %Y',
+
+    # 'October 25 2006', 'October 25, 2006'
+    '%B %d %Y', '%B %d, %Y',
+
+    # '25 October 2006', '25 October, 2006'
+    '%d %B %Y', '%d %B, %Y',
 ]
 
 STATES = (
@@ -299,5 +312,7 @@ COUNTRIES = (
     ('ZM', 'Zambia'),
     ('ZW', 'Zimbabwe'),
 )
-    
-ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+ALPHABET = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']

karaage/common/context_processors.py
@@ -19,6 +19,7 @@ from django.conf import settings
 
 from karaage.common import is_admin, get_hooks
 
+
 def common(request):
     """ Set context with common variables. """
     ctx = {}

karaage/common/create_update.py
@@ -19,6 +19,7 @@ def apply_extra_context(extra_context, context):
         else:
             context[key] = value
 
+
 def get_model_and_form_class(model, form_class):
     """
     Returns a model and form class based on the model and form_class
@@ -36,13 +37,17 @@ def get_model_and_form_class(model, form_class):
         tmp_model = model
         # TODO: we should be able to construct a ModelForm without creating
         # and passing in a temporary inner class.
+
         class Meta:
             model = tmp_model
+
         class_name = model.__name__ + 'Form'
-        form_class = ModelFormMetaclass(class_name, (ModelForm,), {'Meta': Meta})
+        form_class = ModelFormMetaclass(
+            class_name, (ModelForm,), {'Meta': Meta})
         return model, form_class
     raise GenericViewError("Generic view must be called with either a model or"
                            " form_class argument.")
+
 
 def redirect(post_save_redirect, obj):
     """
@@ -68,6 +73,7 @@ def redirect(post_save_redirect, obj):
             " parameter to the generic view or define a get_absolute_url"
             " method on the Model.")
 
+
 def lookup_object(model, object_id, slug, slug_field):
     """
     Return the ``model`` object with the passed ``object_id``.  If
@@ -90,7 +96,9 @@ def lookup_object(model, object_id, slug, slug_field):
         raise Http404("No %s found for %s"
                       % (model._meta.verbose_name, lookup_kwargs))
 
-def create_object(request, model=None, template_name=None,
+
+def create_object(
+        request, model=None, template_name=None,
         template_loader=loader, extra_context=None, post_save_redirect=None,
         login_required=False, context_processors=None, form_class=None):
     """
@@ -101,7 +109,8 @@ def create_object(request, model=None, template_name=None,
         form
             the form for the object
     """
-    if extra_context is None: extra_context = {}
+    if extra_context is None:
+        extra_context = {}
     if login_required and not request.user.is_authenticated():
         return redirect_to_login(request.path)
 
@@ -112,7 +121,7 @@ def create_object(request, model=None, template_name=None,
             new_object = form.save()
 
             msg = ugettext("The %(verbose_name)s was created successfully.") %\
-                                    {"verbose_name": model._meta.verbose_name}
+                {"verbose_name": model._meta.verbose_name}
             messages.success(request, msg, fail_silently=True)
             return redirect(post_save_redirect, new_object)
     else:
@@ -120,7 +129,8 @@ def create_object(request, model=None, template_name=None,
 
     # Create the template, context, response
     if not template_name:
-        template_name = "%s/%s_form.html" % (model._meta.app_label, model._meta.object_name.lower())
+        template_name = "%s/%s_form.html" % (
+            model._meta.app_label, model._meta.object_name.lower())
     t = template_loader.get_template(template_name)
     c = RequestContext(request, {
         'form': form,
@@ -128,7 +138,9 @@ def create_object(request, model=None, template_name=None,
     apply_extra_context(extra_context, c)
     return HttpResponse(t.render(c))
 
-def update_object(request, model=None, object_id=None, slug=None,
+
+def update_object(
+        request, model=None, object_id=None, slug=None,
         slug_field='slug', template_name=None, template_loader=loader,
         extra_context=None, post_save_redirect=None, login_required=False,
         context_processors=None, template_object_name='object',
@@ -143,7 +155,8 @@ def update_object(request, model=None, object_id=None, slug=None,
         object
             the original object being edited
     """
-    if extra_context is None: extra_context = {}
+    if extra_context is None:
+        extra_context = {}
     if login_required and not request.user.is_authenticated():
         return redirect_to_login(request.path)
 
@@ -155,14 +168,15 @@ def update_object(request, model=None, object_id=None, slug=None,
         if form.is_valid():
             obj = form.save()
             msg = ugettext("The %(verbose_name)s was updated successfully.") %\
-                                    {"verbose_name": model._meta.verbose_name}
+                {"verbose_name": model._meta.verbose_name}
             messages.success(request, msg, fail_silently=True)
             return redirect(post_save_redirect, obj)
     else:
         form = form_class(instance=obj)
 
     if not template_name:
-        template_name = "%s/%s_form.html" % (model._meta.app_label, model._meta.object_name.lower())
+        template_name = "%s/%s_form.html" % (
+            model._meta.app_label, model._meta.object_name.lower())
     t = template_loader.get_template(template_name)
     c = RequestContext(request, {
         'form': form,
@@ -172,7 +186,9 @@ def update_object(request, model=None, object_id=None, slug=None,
     response = HttpResponse(t.render(c))
     return response
 
-def delete_object(request, model, post_delete_redirect, object_id=None,
+
+def delete_object(
+        request, model, post_delete_redirect, object_id=None,
         slug=None, slug_field='slug', template_name=None,
         template_loader=loader, extra_context=None, login_required=False,
         context_processors=None, template_object_name='object'):
@@ -188,7 +204,8 @@ def delete_object(request, model, post_delete_redirect, object_id=None,
         object
             the original object being deleted
     """
-    if extra_context is None: extra_context = {}
+    if extra_context is None:
+        extra_context = {}
     if login_required and not request.user.is_authenticated():
         return redirect_to_login(request.path)
 
@@ -197,12 +214,13 @@ def delete_object(request, model, post_delete_redirect, object_id=None,
     if request.method == 'POST':
         obj.delete()
         msg = ugettext("The %(verbose_name)s was deleted.") %\
-                                    {"verbose_name": model._meta.verbose_name}
+            {"verbose_name": model._meta.verbose_name}
         messages.success(request, msg, fail_silently=True)
         return HttpResponseRedirect(post_delete_redirect)
     else:
         if not template_name:
-            template_name = "%s/%s_confirm_delete.html" % (model._meta.app_label, model._meta.object_name.lower())
+            template_name = "%s/%s_confirm_delete.html" % (
+                model._meta.app_label, model._meta.object_name.lower())
         t = template_loader.get_template(template_name)
         c = RequestContext(request, {
             template_object_name: obj,

karaage/common/decorators.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.contrib.auth.decorators import user_passes_test
 from django.core.exceptions import PermissionDenied
 
+
 def admin_required(function=None):
     """
     Decorator for views that checks that the user is an administrator,
@@ -119,11 +120,9 @@ def xmlrpc_machine_required(function=None):
 
         if func.__doc__:
             wrapper.__doc__ = func.__doc__ + \
-                    "\nNote: Machine authentication is required."
+                "\nNote: Machine authentication is required."
         return wrapper
 
     if function:
         return actual_decorator(function)
     return actual_decorator
-
-

karaage/common/emails.py
@@ -18,13 +18,9 @@
 """
 All email sending is done from this module
 """
-__author__ = 'Sam Morrison'
-
 from django.core.mail import EmailMessage
-from django.template.loader import render_to_string
 from django.conf import settings
 
-from karaage.common import log
 
 CONTEXT = {
     'org_email': settings.ACCOUNTS_EMAIL,
@@ -39,5 +35,5 @@ def send_mail(subject, message, from_email, recipient_list):
     }
 
     email = EmailMessage(subject, message, from_email, recipient_list,
-                        headers=headers)
+                         headers=headers)
     return email.send()

commit 1d54268b1f27933d277415b254dc490a529b701a

Fix breakage introduced in PEP8 cleanup
Change bad98d8d71441121cec62a5c0d5b1fe1eca0dc5e accidentally turned
values into tuples.

Change-Id: I05e278a6892d8990c7388b368d6366b085810031

karaage/common/views/common.py
@@ -35,14 +35,14 @@ from karaage.projects.models import Project
 def admin_index(request):
     newest_users = Person.objects.order_by('-date_approved', '-id')
     newest_users = newest_users.filter(date_approved__isnull=False)
-    newest_users = newest_users.select_related()[:5],
+    newest_users = newest_users.select_related()[:5]
 
     newest_projects = Project.objects.order_by('-date_approved')
     newest_projects = newest_projects.filter(date_approved__isnull=False)
     newest_projects = newest_projects.filter(is_active=True)
-    newest_projects = newest_projects.select_related()[:5],
+    newest_projects = newest_projects.select_related()[:5]
 
-    recent_actions = request.user.logentry_set.all()[:10],
+    recent_actions = request.user.logentry_set.all()[:10]
 
     var = {
         'newest_users': newest_users,