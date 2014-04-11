e67cf22f9b991f98b471596afeac4cdcfefe99a3
Brian May <brian@microcomaustralia.com.au>
2014-04-11 14:29:43 +1000

    Use python-pipeline to compress css and js files.
    Render all css and js files into one that can be loaded into browser and
    cached between requests.
    
    Change-Id: Ie6460b88dd8a0940068b6a6b37b1b5fe1061e9a4

--- karaage/common/static/ajax_select/js/bootstrap.js
+++
@@ -1,8 +0,0 @@
-// load jquery and jquery-ui if needed
-// into window.jQuery
-if (typeof window.jQuery === 'undefined') {
-  document.write('<script type="text/javascript"  src="' + window.__static_prefix__ + 'js/jquery-1.11.0.js"><\/script><script type="text/javascript"  src="' + window.__static_prefix__ + 'js/jquery-ui-1.10.4.js"><\/script><link type="text/css" rel="stylesheet" href="' + window.__static_prefix__ + 'css/jquery-ui.css" />');
-} else if(typeof window.jQuery.ui === 'undefined' || typeof window.jQuery.ui.autocomplete === 'undefined') {
-  document.write('<script type="text/javascript"  src="' + window.__static_prefix__ + 'js/jquery-ui-1.10.4.js"><\/script><link type="text/css" rel="stylesheet" href="' + window.__static_prefix__ + 'css/jquery-ui.css" />');
-}
-
