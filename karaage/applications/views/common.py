aaf7a70083ae60bcad21b822612377e57935ec19
Brian May <brian@microcomaustralia.com.au>
2014-04-17 16:52:07 +1000

    Use django-filter/django-tables2 for applications.
    Change-Id: I906dec7e7b16c19504594ff523fc9e03c6cfe485

--- karaage/applications/views/common.py
+++ karaage/applications/views/common.py
@@ -15,17 +15,17 @@
 # You should have received a copy of the GNU General Public License
 # along with Karaage  If not, see <http://www.gnu.org/licenses/>.
 
+import django_tables2 as tables
 import datetime
 
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.contrib import messages
-from django.db.models import Q
 from django.http import HttpResponseRedirect
 
 from karaage.common.decorators import admin_required, login_required
+from karaage.applications.tables import ApplicationFilter, ApplicationTable
 from karaage.applications.models import Applicant, Application
 from karaage.applications.forms import ApplicantForm
 import karaage.applications.views.base as base
@@ -34,41 +34,32 @@ import karaage.common as util
 
 @login_required
 def application_list(request):
-    """ a logged in user wants to see all his pending applications. """
-    applications = Application.objects.all()
+    """ a user wants to see all applications possible. """
 
-    if not util.is_admin(request):
-        applications = Application.objects.get_for_applicant(request.user)
-
-    if 'search' in request.REQUEST:
-        terms = request.REQUEST['search'].lower()
-        query = Q()
-
-        if terms:
-            for term in terms.split(' '):
-                q = Q(projectapplication__project__pid__icontains=term)
-                q = q | Q(projectapplication__project__name__icontains=term)
-                q = q | Q(softwareapplication__software_license__software__name__icontains=term)
-                query = query & q
-
-        applications = applications.filter(query)
+    if util.is_admin(request):
+        queryset = Application.objects.all()
     else:
-        terms = ""
+        queryset = Application.objects.get_for_applicant(request.user)
 
-    page_no = request.GET.get('page')
-    paginator = Paginator(applications, 50)
-    try:
-        page = paginator.page(page_no)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        page = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        page = paginator.page(paginator.num_pages)
+    filter = ApplicationFilter(request.GET, queryset=queryset)
+
+    table = ApplicationTable(filter.qs)
+    tables.RequestConfig(request).configure(table)
+
+    spec = []
+    for name, value in filter.form.cleaned_data.iteritems():
+        if value is not None and value != "":
+            name = name.replace('_', ' ').capitalize()
+            spec.append((name, value))
 
     return render_to_response(
         "applications/application_list.html",
-        {'page': page, 'terms': terms},
+        {
+            'table': table,
+            'filter': filter,
+            'spec': spec,
+            'title': "Application list",
+        },
         context_instance=RequestContext(request))
 
 
