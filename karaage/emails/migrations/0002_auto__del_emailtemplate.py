36746d52cf6c6de576f8107e5eb702a869bc504f
Brian May <brian@microcomaustralia.com.au>
2014-04-29 13:07:26 +1000

    Remove legacy db table.
    Was causing problems for logs, as this model didn't provide a
    get_absolete_url method, required for display of the LogEntry table.
    
    Change-Id: I5269ce135b0a10c89418bee41327ff5abd0cd4ce

---
+++ karaage/emails/migrations/0002_auto__del_emailtemplate.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from south.db import db
+from south.v2 import SchemaMigration
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting model 'EmailTemplate'
+        db.delete_table(u'emails_emailtemplate')
+
+        if not db.dry_run:
+            orm['contenttypes.contenttype'].objects.filter(app_label='emails').delete()
+
+    def backwards(self, orm):
+        # Adding model 'EmailTemplate'
+        db.create_table(u'emails_emailtemplate', (
+            ('body', self.gf('django.db.models.fields.TextField')()),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('subject', self.gf('django.db.models.fields.CharField')(max_length=100)),
+        ))
+        db.send_create_signal(u'emails', ['EmailTemplate'])
+
+    models = {
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+    }
+
+    complete_apps = ['emails']
