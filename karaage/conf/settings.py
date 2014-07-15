8a66ecf636e323fe1637a5c0371927586bc59f44
Brian May <brian@microcomaustralia.com.au>
2014-07-15 11:17:32 +1000

    Split usage stuff into kgusage.
    Change-Id: I450e4694238b81e95d41c8a70193af70f6e8dcb9

--- karaage/conf/settings.py
+++ karaage/conf/settings.py
@@ -1,5 +1,26 @@
 from karaage.conf.defaults import *
 
+PLUGINS = []
+
 exec(open("/etc/karaage3/settings.py", "rb").read())
 
 DEFAULT_FROM_EMAIL = ACCOUNTS_EMAIL
+
+
+def load_plugins(plugins):
+    global INSTALLED_APPS
+    global XMLRPC_METHODS
+
+    import importlib
+    for plugin in plugins:
+        module_name, descriptor_name = plugin.rsplit(".", 1)
+        module = importlib.import_module(module_name)
+        descriptor = getattr(module, descriptor_name)
+        assert descriptor.plugin == "karaage3"
+
+        INSTALLED_APPS = (descriptor.module,) + descriptor.requires \
+            + INSTALLED_APPS
+
+        XMLRPC_METHODS = descriptor.xmlrpc_methods + XMLRPC_METHODS
+
+load_plugins(PLUGINS)
