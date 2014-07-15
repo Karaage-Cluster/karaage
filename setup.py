df42d1b5d52cfd8eff4ed860b54f5174e40a644c
Brian May <brian@microcomaustralia.com.au>
2014-07-15 11:14:34 +1000

    Fix copyright declaration.
    Change-Id: Ie46e4862a45f2ad568e6321f513d40290d2e7c6b

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 
 # Copyright 2012-2014 VPAC
 #
-# This file is part of django-tldap.
+# This file is part of Karaage.
 #
-# django-tldap is free software: you can redistribute it and/or modify
+# Karaage is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# django-tldap is distributed in the hope that it will be useful,
+# Karaage is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with django-tldap  If not, see <http://www.gnu.org/licenses/>.
+# along with Karaage  If not, see <http://www.gnu.org/licenses/>.
 
 from setuptools import setup
 import os
