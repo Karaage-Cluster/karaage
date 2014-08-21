4bdbce10e8e844f35af7fac8b45a71e6ebdcc0b4
Brian May <brian@microcomaustralia.com.au>
2014-08-21 14:29:27 +1000

    Fix unicode strings.
    Change-Id: I433e8fabe66af842d238b439789f1a0c1b25b252

--- kgapplications/south_migrations/0001_initial.py
+++ kgapplications/south_migrations/0001_initial.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
@@ -13,14 +12,14 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         if not db.dry_run:
             orm['contenttypes.contenttype'].objects.filter(app_label='applications').update(app_label='kgapplications')
-        db.send_create_signal(u'kgapplications', ['Application'])
-        db.send_create_signal(u'kgapplications', ['ProjectApplication'])
-        db.send_create_signal(u'kgapplications', ['Applicant'])
+        db.send_create_signal('kgapplications', ['Application'])
+        db.send_create_signal('kgapplications', ['ProjectApplication'])
+        db.send_create_signal('kgapplications', ['Applicant'])
         return
 
         # Adding model 'Application'
-        db.create_table(u'kgapplications_application', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table('kgapplications_application', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('secret_token', self.gf('django.db.models.fields.CharField')(default='42c3a931174e42f6c76f41056ecad8d9c18aabd0', unique=True, max_length=64)),
             ('expires', self.gf('django.db.models.fields.DateTimeField')()),
             ('created_by', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['karaage.Person'], null=True, blank=True)),
@@ -33,11 +32,11 @@ class Migration(SchemaMigration):
             ('header_message', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
             ('_class', self.gf('django.db.models.fields.CharField')(max_length=100)),
         ))
-        db.send_create_signal(u'kgapplications', ['Application'])
+        db.send_create_signal('kgapplications', ['Application'])
 
         # Adding model 'ProjectApplication'
-        db.create_table(u'kgapplications_projectapplication', (
-            (u'application_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['kgapplications.Application'], unique=True, primary_key=True)),
+        db.create_table('kgapplications_projectapplication', (
+            ('application_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['kgapplications.Application'], unique=True, primary_key=True)),
             ('needs_account', self.gf('django.db.models.fields.BooleanField')(default=True)),
             ('make_leader', self.gf('django.db.models.fields.BooleanField')(default=False)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
@@ -47,20 +46,20 @@ class Migration(SchemaMigration):
             ('pid', self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True)),
             ('project', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['karaage.Project'], null=True, blank=True)),
         ))
-        db.send_create_signal(u'kgapplications', ['ProjectApplication'])
+        db.send_create_signal('kgapplications', ['ProjectApplication'])
 
         # Adding M2M table for field machine_categories on 'ProjectApplication'
-        m2m_table_name = db.shorten_name(u'kgapplications_projectapplication_machine_categories')
+        m2m_table_name = db.shorten_name('kgapplications_projectapplication_machine_categories')
         db.create_table(m2m_table_name, (
             ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('projectapplication', models.ForeignKey(orm[u'kgapplications.projectapplication'], null=False)),
+            ('projectapplication', models.ForeignKey(orm['kgapplications.projectapplication'], null=False)),
             ('machinecategory', models.ForeignKey(orm['karaage.machinecategory'], null=False))
         ))
         db.create_unique(m2m_table_name, ['projectapplication_id', 'machinecategory_id'])
 
         # Adding model 'Applicant'
-        db.create_table(u'kgapplications_applicant', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table('kgapplications_applicant', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('email', self.gf('django.db.models.fields.EmailField')(unique=True, max_length=75)),
             ('email_verified', self.gf('django.db.models.fields.BooleanField')(default=False)),
             ('username', self.gf('django.db.models.fields.CharField')(max_length=255, unique=True, null=True, blank=True)),
@@ -80,7 +79,7 @@ class Migration(SchemaMigration):
             ('fax', self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True)),
             ('saml_id', self.gf('django.db.models.fields.CharField')(max_length=200, unique=True, null=True, blank=True)),
         ))
-        db.send_create_signal(u'kgapplications', ['Applicant'])
+        db.send_create_signal('kgapplications', ['Applicant'])
 
 
     def backwards(self, orm):
@@ -89,23 +88,23 @@ class Migration(SchemaMigration):
         return
 
         # Deleting model 'Application'
-        db.delete_table(u'kgapplications_application')
+        db.delete_table('kgapplications_application')
 
         # Deleting model 'ProjectApplication'
-        db.delete_table(u'kgapplications_projectapplication')
+        db.delete_table('kgapplications_projectapplication')
 
         # Removing M2M table for field machine_categories on 'ProjectApplication'
-        db.delete_table(db.shorten_name(u'kgapplications_projectapplication_machine_categories'))
+        db.delete_table(db.shorten_name('kgapplications_projectapplication_machine_categories'))
 
         # Deleting model 'Applicant'
-        db.delete_table(u'kgapplications_applicant')
+        db.delete_table('kgapplications_applicant')
 
 
     models = {
-        u'contenttypes.contenttype': {
+        'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
@@ -114,7 +113,7 @@ class Migration(SchemaMigration):
             'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'extra_data': ('jsonfield.fields.JSONField', [], {'default': '{}'}),
             'foreign_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'unique': 'True', 'null': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'members': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'groups'", 'symmetrical': 'False', 'to': "orm['karaage.Person']"}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
         },
@@ -122,14 +121,14 @@ class Migration(SchemaMigration):
             'Meta': {'ordering': "['name']", 'object_name': 'Institute', 'db_table': "'institute'"},
             'delegates': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'delegate_for'", 'to': "orm['karaage.Person']", 'through': "orm['karaage.InstituteDelegate']", 'blank': 'True', 'symmetrical': 'False', 'null': 'True'}),
             'group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['karaage.Group']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
             'saml_entityid': ('django.db.models.fields.CharField', [], {'max_length': '200', 'unique': 'True', 'null': 'True', 'blank': 'True'})
         },
         'karaage.institutedelegate': {
             'Meta': {'object_name': 'InstituteDelegate', 'db_table': "'institutedelegate'"},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'institute': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['karaage.Institute']"}),
             'person': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['karaage.Person']"}),
             'send_email': ('django.db.models.fields.BooleanField', [], {})
@@ -137,7 +136,7 @@ class Migration(SchemaMigration):
         'karaage.machinecategory': {
             'Meta': {'object_name': 'MachineCategory', 'db_table': "'machine_category'"},
             'datastore': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
         },
         'karaage.person': {
@@ -155,7 +154,7 @@ class Migration(SchemaMigration):
             'expires': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
             'fax': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
             'full_name': ('django.db.models.fields.CharField', [], {'max_length': '60'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'institute': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['karaage.Institute']"}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_admin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
@@ -187,7 +186,7 @@ class Migration(SchemaMigration):
             'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'end_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
             'group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['karaage.Group']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'institute': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['karaage.Institute']"}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_approved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
@@ -197,7 +196,7 @@ class Migration(SchemaMigration):
             'pid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
             'start_date': ('django.db.models.fields.DateField', [], {'default': 'datetime.datetime(2014, 8, 15, 0, 0)'})
         },
-        u'kgapplications.applicant': {
+        'kgapplications.applicant': {
             'Meta': {'object_name': 'Applicant'},
             'address': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
             'city': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
@@ -207,7 +206,7 @@ class Migration(SchemaMigration):
             'email_verified': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'fax': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
             'full_name': ('django.db.models.fields.CharField', [], {'max_length': '60'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'institute': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['karaage.Institute']", 'null': 'True', 'blank': 'True'}),
             'mobile': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
             'position': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
@@ -219,25 +218,25 @@ class Migration(SchemaMigration):
             'title': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
             'username': ('django.db.models.fields.CharField', [], {'max_length': '255', 'unique': 'True', 'null': 'True', 'blank': 'True'})
         },
-        u'kgapplications.application': {
+        'kgapplications.application': {
             'Meta': {'object_name': 'Application'},
             '_class': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'complete_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
             'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['karaage.Person']", 'null': 'True', 'blank': 'True'}),
             'created_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'expires': ('django.db.models.fields.DateTimeField', [], {}),
             'header_message': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
             'secret_token': ('django.db.models.fields.CharField', [], {'default': "'5483f8fce468057ae3e56507adeca597222ba479'", 'unique': 'True', 'max_length': '64'}),
             'state': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
             'submitted_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'})
         },
-        u'kgapplications.projectapplication': {
-            'Meta': {'object_name': 'ProjectApplication', '_ormbases': [u'kgapplications.Application']},
+        'kgapplications.projectapplication': {
+            'Meta': {'object_name': 'ProjectApplication', '_ormbases': ['kgapplications.Application']},
             'additional_req': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            u'application_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['kgapplications.Application']", 'unique': 'True', 'primary_key': 'True'}),
+            'application_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['kgapplications.Application']", 'unique': 'True', 'primary_key': 'True'}),
             'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'institute': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['karaage.Institute']", 'null': 'True', 'blank': 'True'}),
             'machine_categories': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['karaage.MachineCategory']", 'null': 'True', 'blank': 'True'}),
