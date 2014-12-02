d28531694423f7b94db9455812c333e14d90c57e
Sky <sky@commoncode.com.au>
2014-12-02 12:58:43 +1100

    Set up models for grants and allocations

---
+++ karaage/allocations/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+from audit_log.models.managers import AuditLog
+
+from projects.models import Project
+from resources.models import ResourcePool
+from schemes.models import Scheme
+
+
+class Allocation(models.Model):
+    grant = models.ForeignKey(Grant)
+    project = models.ForeignKey(Project)
+    resource_pool = models.ForeignKey(ResourcePool)
+    quantity = models.FloatField()
+
+    audit_log = AuditLog()
+
+
+class Grant(models.Model):
+    project = models.ForeignKey(Project)
+    scheme = models.ForeignKey(Scheme)
+    description = models.CharField(max_length=255)
+    date = models.DateField()
+    begins = models.DateField()
+    expires = models.DateField()
+
+    audit_log = AuditLog()
