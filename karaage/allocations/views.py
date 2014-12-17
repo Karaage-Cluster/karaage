f3978495dffb5ee56e2e4c79035a5513ce7f6518
Sky <sky@commoncode.com.au>
2014-12-17 11:13:11 +1100

    Grants can be edited from the project default view

--- karaage/allocations/views.py
+++ karaage/allocations/views.py
@@ -5,7 +5,10 @@ from django.shortcuts import (
     redirect,
 )
 
-from karaage.allocations.models import Allocation
+from karaage.allocations.models import (
+    Allocation,
+    Grant,
+)
 from karaage.allocations.forms import (
     AllocationForm,
     AllocationPeriodForm,
@@ -75,27 +78,38 @@ def add_edit_allocation(request, project_id, allocation_id=None):
 
 
 @admin_required
-def grant_add(request, project_id):
+def add_edit_grant(request, project_id, grant_id=None):
 
     project = Project.objects.get(pk=project_id)
 
+    if grant_id:
+        title = 'Edit grant'
+        grant = Grant.objects.get(pk=grant_id)
+    else:
+        title = 'Add grant'
+
     if request.method == "POST":
-        form = GrantForm(request.POST)
+        if grant_id:
+            form = GrantForm(request.POST, instance=grant)
+        else:
+            form = GrantForm(request.POST)
         if form.is_valid():
-            # import pdb; pdb.set_trace()
             obj = form.save(commit=False)
             obj.project = project
             obj.save()
             return redirect('kg_project_detail', project_id)
     else:
-        form = GrantForm()
+        if grant_id:
+            form = GrantForm(instance=grant)
+        else:
+            form = GrantForm()
 
     return render(
         request,
-        'karaage/allocations/allocation_add_template.html',
+        'karaage/allocations/allocation_add_edit_template.html',
         {
             'form': form,
-            'title': 'Add grant',
+            'title': title,
         },
     )
 
