558b04230650c32607d28081f4c0dacea3c94741
Brian May <brian@microcomaustralia.com.au>
2015-02-05 15:45:48 +1100

    Test all pages are accessible.
    Change-Id: I832b34db7476cbfe0e4efb52de3fa0312ba2322b

---
+++ karaage/tests/test_all_pages.py
@@ -0,0 +1,44 @@
+# Copyright 2014 VLSCI
+# Modifications Copyright 2015 VPAC
+#
+# This file is part of Karaage.
+#
+# Karaage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Karaage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Karaage  If not, see <http://www.gnu.org/licenses/>.
+
+"""Test all pages render (without exceptions)."""
+
+from __future__ import print_function, unicode_literals
+from .client import TestAllPagesCase
+
+
+class TestKaraagePages(TestAllPagesCase):
+
+    """Discover all URLs, do a HTTP GET and
+    confirm 200 OK and no DB changes."""
+
+    fixtures = [
+        'test_karaage.json'
+    ]
+    variables = {
+        'username': 'kgsuper',
+        'group_name': 'example',
+        'account_id': '1',
+        'project_id': 'TestProject1',
+        'projectquota_id': '1',
+        'institute_id': '1',
+        'institutequota_id': '1',
+        'machine_id': '1',
+        'category_id': '1',
+    }
+    module = "karaage"
