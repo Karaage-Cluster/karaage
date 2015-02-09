798d28c01bc8a31c67f5783731ec5a8a17b91319
Brian May <brian@microcomaustralia.com.au>
2015-02-09 14:20:43 +1100

    Make generic TestAllPagesCase class.

---
+++ karaage/tests/client.py
@@ -0,0 +1,119 @@
+# Copyright 2015 VPAC
+#
+# This file is part of Karaage.
+#
+# Karaage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Karaage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Karaage  If not, see <http://www.gnu.org/licenses/>.
+
+"""Test all pages render (without exceptions)."""
+
+from __future__ import print_function, unicode_literals
+
+import six
+import re
+import unittest
+
+from django.conf import settings
+from django.contrib.admindocs.views import simplify_regex
+from django.test import TestCase
+from django.utils.text import slugify
+from django.utils.encoding import smart_text
+
+from django_extensions.management.commands.show_urls import \
+    extract_views_from_urlpatterns
+
+urlconf = __import__(settings.ROOT_URLCONF, {}, {}, [''])
+
+
+def make_test_get_function(name, url, url_pattern):
+    def test_get(self):
+        self.assertEqual(
+            self.client.login(username='kgsuper', password='aq12ws'),
+            True,
+            'Login failed.',
+        )
+        resp = self.client.get(url, follow=True)
+        self.assertIn(
+            resp.status_code,
+            [200, 400, 403],
+            'HTTP Error {}: {} > {}'.format(
+                resp.status_code,
+                url_pattern,
+                url,
+            ),
+        )
+    test_get.__name__ = str(name)
+    return test_get
+
+
+class TestAllPagesMeta(type):
+    @classmethod
+    def _add_test_methods(cls, attrs, urlpatterns):
+        # loop through every URL pattern
+        for index, (func, regex, url_name) in enumerate(
+                extract_views_from_urlpatterns(urlpatterns)):
+
+            if func.__module__.startswith("%s." % attrs['module']):
+                pass
+            elif func.__module__ == attrs['module']:
+                pass
+            else:
+                continue
+
+            if hasattr(func, '__name__'):
+                func_name = func.__name__
+            elif hasattr(func, '__class__'):
+                func_name = '%s()' % func.__class__.__name__
+            else:
+                func_name = re.sub(r' at 0x[0-9a-f]+', '', repr(func))
+
+            url_pattern = smart_text(simplify_regex(regex))
+            name = '_'.join(
+                [
+                    'test',
+                    func.__module__.replace('.', '_'),
+                    slugify('%s' % func_name),
+                ] + slugify(
+                    url_pattern.replace('/', '_') or 'root'
+                ).replace('_', ' ').split(),
+            )
+            url = url_pattern
+
+            for key, value in attrs['variables'].items():
+                url = url.replace('<%s>' % key, value)
+
+            # bail out if we don't know how to visit this URL properly
+            testfunc = unittest.skipIf(
+                any(
+                    re.search(stop_pattern, url)
+                    for stop_pattern
+                    in [
+                        r'<.*>',
+                    ]
+                ),
+                'URL pattern %r contains stop pattern.' % url,
+            )(
+                make_test_get_function(name, url, url_pattern),
+            )
+
+            attrs[name] = testfunc
+
+    def __new__(cls, name, parents, attrs):
+        if parents != (TestCase,):
+            cls._add_test_methods(attrs, urlconf.urlpatterns)
+        return super(TestAllPagesMeta, cls).__new__(cls, name, parents, attrs)
+
+
+@six.add_metaclass(TestAllPagesMeta)
+class TestAllPagesCase(TestCase):
+    pass
--- karaage/tests/test_all_pages.py
+++ karaage/tests/test_all_pages.py
@@ -1,138 +1,43 @@
-"""Test all pages render (without exceptions) using Selenium."""
+# Copyright 2014 The University of Melbourne
+#
+# This file is part of Karaage.
+#
+# Karaage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Karaage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Karaage  If not, see <http://www.gnu.org/licenses/>.
+
+"""Test all pages render (without exceptions)."""
+
 from __future__ import print_function, unicode_literals
-
-import functools
-import re
-import unittest
-
-from django.conf import settings
-from django.contrib.admindocs.views import simplify_regex
-from django.test import TestCase
-from django.utils.text import slugify
-from django.utils.encoding import smart_text
-
-from django_extensions.management.commands.show_urls import \
-    extract_views_from_urlpatterns
+from .client import TestAllPagesCase
 
 
-def make_test_get_function(name, url, url_pattern):
-    def test_get(self):
-        self.assertEqual(
-            self.client.login(username='1', password='password'),
-            True,
-            'Login failed.',
-        )
-        resp = self.client.get(url)
-        self.assertIn(
-            resp.status_code,
-            [200, 302, 403],
-            'HTTP Error {}: {} > {}'.format(
-                resp.status_code,
-                url_pattern,
-                url,
-            ),
-        )
-    test_get.__name__ = str(name)
-    return test_get
+class TestKaraagePages(TestAllPagesCase):
 
-
-class TestAllPages(TestCase):
-
-    """Discover all URLs, do a HTTP GET and confirm 200 OK and no DB changes."""
+    """Discover all URLs, do a HTTP GET and
+    confirm 200 OK and no DB changes."""
 
     fixtures = [
-        fixture
-        for (app, fixture)
-        in [
-            ('karaage.apps.Karaage', 'test_karaage.json'),
-            ('kgapplications.plugin', 'test_kgapplications.json'),
-            ('kgsoftware.plugin', 'test_kgsoftware.json'),
-            ('kgusage.plugin', 'test_kgusage.json'),
-        ]
-        if app in settings.INSTALLED_APPS
+        'test_karaage.json'
     ]
-
-
-urlconf = __import__(settings.ROOT_URLCONF, {}, {}, [''])
-
-
-def add_test_methods(testcase, urlpatterns):
-    decorator = ['login_required']
-    # loop through every URL pattern
-    for index, (func, regex, url_name) in enumerate(
-        extract_views_from_urlpatterns(urlpatterns)
-    ):
-        if hasattr(func, '__globals__'):
-            func_globals = func.__globals__
-        elif hasattr(func, 'func_globals'):
-            func_globals = func.func_globals
-        else:
-            func_globals = {}
-
-        decorators = [d for d in decorator if d in func_globals]
-
-        if isinstance(func, functools.partial):
-            func = func.func
-            decorators.insert(0, 'functools.partial')
-
-        if hasattr(func, '__name__'):
-            func_name = func.__name__
-        elif hasattr(func, '__class__'):
-            func_name = '%s()' % func.__class__.__name__
-        else:
-            func_name = re.sub(r' at 0x[0-9a-f]+', '', repr(func))
-
-        url_pattern = smart_text(simplify_regex(regex))
-        name = '_'.join(
-            [
-                'test',
-                '%.4d' % index,
-                func.__module__.replace(u'.', u'_'),
-                slugify(u'%s' % func_name),
-            ] + slugify(
-                url_pattern.replace(u'/', u'_') or u'root'
-            ).replace(u'_', u' ').split(),
-        )
-        url = re.sub('<[\w_]+>', '1', url_pattern)
-        # bail out if we don't know how to visit this URL properly
-        testfunc = unittest.skipIf(
-            any(
-                re.search(stop_pattern, url_pattern)
-                for stop_pattern
-                in [
-                    r'<uidb64>',
-                    #r'<app_label>',
-                    #r'<model>',
-                    #r'<key>',
-                    #r'<token>',
-                    #r'/tasks/',
-                    #r'/usage/',
-                    #r'/captcha/',
-                ]
-            ),
-            'URL pattern %r contains stop pattern.' % url_pattern,
-        )(
-            make_test_get_function(name, url, url_pattern),
-        )
-        view_path = '%s.%s' % (func.__module__, func_name)
-        testfunc = unittest.skipIf(
-            any(
-                re.search(stop_pattern, view_path)
-                for stop_pattern in [
-                    r'^captcha\.',
-                    r'^djcelery\.',
-                    r'^ajax_select\.',
-                    r'^django\.contrib\.auth\.views\.',
-                    r'^django\.contrib\.staticfiles\.views\.',
-                    r'^django_xmlrpc\.views\.',
-                    r'^karaage\.people\.views\.persons\.activate$',
-                    r'^kgusage\.views\.',
-                ]
-            ),
-            'View %r contains stop pattern.' % view_path,
-        )(
-            testfunc,
-        )
-        setattr(testcase, name, testfunc)
-
-add_test_methods(TestAllPages, urlconf.urlpatterns)
+    variables = {
+        'username': 'kgsuper',
+        'group_name': 'example',
+        'account_id': '1',
+        'project_id': 'TestProject1',
+        'projectquota_id': '1',
+        'institute_id': '1',
+        'institutequota_id': '1',
+        'machine_id': '1',
+        'category_id': '1',
+    }
+    module = "karaage"
