Karaage-Cluster/karaage

commit 248e29877007dce1fcc2385057fe413f795df013

Use numeric project_id in al project URLs.
Previously, old code used the not numeric string-id value, while new
code used numeric project_ids.

This was inconsistant and also broken.

Using numeric ids is better, as we don't have to worry about different
sites having different PROJECT_VALIDATION_RE values in the URLS.

karaage/projects/tables.py
@@ -57,7 +57,7 @@ class ProjectColumn(BaseLinkColumn):
 
     def render(self, value):
         if value is not None:
-            url = reverse('kg_project_detail', args=[value.pid])
+            url = reverse('kg_project_detail', args=[value.id])
             link = self.render_link(url, text=six.text_type(value.pid))
             return link
         else:
@@ -76,8 +76,9 @@ class ProjectFilter(django_filters.FilterSet):
 
 class ProjectTable(tables.Table):
     is_active = tables.Column(order_by=('-is_active'), verbose_name="active")
-    pid = tables.LinkColumn(
-        'kg_project_detail', args=[A('pid')], verbose_name="PID")
+    id = tables.LinkColumn(
+        'kg_project_detail', args=[A('id')], verbose_name="ID")
+    pid = tables.Column(verbose_name="PID")
     institute = tables.LinkColumn(
         'kg_institute_detail', args=[A('institute.pk')])
     leaders = PeopleColumn(orderable=False)

karaage/machines/tables.py
@@ -56,7 +56,7 @@ class AccountTable(tables.Table):
     username = tables.LinkColumn(
         'kg_account_detail', args=[A('pk')], verbose_name="Account")
     default_project = tables.LinkColumn(
-        'kg_project_detail', args=[A('default_project.pid')])
+        'kg_project_detail', args=[A('default_project.id')])
 
     def render_active(self, record):
         if record.date_deleted is not None:

karaage/allocations/views.py
@@ -41,6 +41,8 @@ from karaage.projects.models import Project
 @admin_required
 def allocation_period_add(request, project_id):
 
+    project = Project.objects.get(pk=project_id)
+
     if request.method == "POST":
         form = AllocationPeriodForm(request.POST)
         if form.is_valid():
@@ -53,7 +55,7 @@ def allocation_period_add(request, project_id):
         request,
         'karaage/allocations/allocation_add_edit_template.html',
         {
-            'pid': project_id,
+            'project': project,
             'form': form,
             'title': 'Add allocation period',
         },
@@ -105,7 +107,7 @@ def add_edit_allocation(request, project_id, allocation_id=None):
         {
             'mode': mode,
             'record_type': 'allocation',
-            'pid': project_id,
+            'project': project,
             'record_id': allocation_id,
             'form': form,
             'title': title,
@@ -116,6 +118,7 @@ def add_edit_allocation(request, project_id, allocation_id=None):
 @admin_required
 def delete_allocation(request, project_id, allocation_id):
 
+    project = Project.objects.get(pk=project_id)
     record = get_object_or_404(Allocation, pk=allocation_id)
 
     if request.method == 'POST':
@@ -126,6 +129,7 @@ def delete_allocation(request, project_id, allocation_id):
         request,
         'karaage/allocations/allocation_confirm_delete_template.html',
         {
+            'project': project,
             'record': record,
             'record_type': 'allocation',
         },
@@ -167,7 +171,7 @@ def add_edit_grant(request, project_id, grant_id=None):
         {
             'mode': mode,
             'record_type': 'grant',
-            'pid': project_id,
+            'project': project,
             'record_id': grant_id,
             'form': form,
             'title': title,
@@ -177,6 +181,8 @@ def add_edit_grant(request, project_id, grant_id=None):
 
 @admin_required
 def delete_grant(request, project_id, grant_id):
+
+    project = Project.objects.get(pk=project_id)
 
     record = get_object_or_404(Grant, pk=grant_id)
     record_type = 'grant'
@@ -197,6 +203,7 @@ def delete_grant(request, project_id, grant_id):
         request,
         'karaage/allocations/allocation_confirm_delete_template.html',
         {
+            'project': project,
             'record': record,
             'record_type': record_type,
             'errors': errors,
@@ -206,6 +213,8 @@ def delete_grant(request, project_id, grant_id):
 
 @admin_required
 def scheme_add(request, project_id):
+
+    project = Project.objects.get(pk=project_id)
 
     if request.method == "POST":
         form = SchemeForm(request.POST)
@@ -219,7 +228,7 @@ def scheme_add(request, project_id):
         request,
         'karaage/allocations/allocation_add_edit_template.html',
         {
-            'pid': project_id,
+            'project': project,
             'form': form,
             'title': 'Add scheme',
         },

karaage/projects/models.py
@@ -147,7 +147,7 @@ class Project(MPTTModel):
 
     @models.permalink
     def get_absolute_url(self):
-        return ('kg_project_detail', [self.pid])
+        return ('kg_project_detail', [self.id])
 
     def save(self, *args, **kwargs):
         created = self.pk is None

karaage/projects/urls.py
@@ -30,28 +30,28 @@ urlpatterns = patterns(
     url(r'^quota/(?P<projectquota_id>\d+)/delete/$',
         'projectquota_delete', name='kg_projectquota_delete'),
 
-    url(r'^(?P<project_id>%s)/$' % settings.PROJECT_VALIDATION_RE,
+    url(r'^(?P<project_id>\d+)/$',
         'project_detail', name='kg_project_detail'),
-    url(r'^(?P<project_id>%s)/verbose/$' % settings.PROJECT_VALIDATION_RE,
+    url(r'^(?P<project_id>\d+)/verbose/$',
         'project_verbose', name='kg_project_verbose'),
-    url(r'^(?P<project_id>%s)/edit/$' % settings.PROJECT_VALIDATION_RE,
+    url(r'^(?P<project_id>\d+)/edit/$',
         'add_edit_project', name='kg_project_edit'),
-    url(r'^(?P<project_id>%s)/delete/$' % settings.PROJECT_VALIDATION_RE,
+    url(r'^(?P<project_id>\d+)/delete/$',
         'delete_project', name='kg_project_delete'),
-    url(r'^(?P<project_id>%s)/remove_user/(?P<username>%s)/$'
-        % (settings.USERNAME_VALIDATION_RE, settings.PROJECT_VALIDATION_RE),
+    url(r'^(?P<project_id>\d+)/remove_user/(?P<username>%s)/$'
+        % (settings.USERNAME_VALIDATION_RE,),
         'remove_user', name='kg_remove_project_member'),
-    url(r'^(?P<project_id>%s)/grant/(?P<username>%s)/$'
-        % (settings.USERNAME_VALIDATION_RE, settings.PROJECT_VALIDATION_RE),
+    url(r'^(?P<project_id>\d+)/grant/(?P<username>%s)/$'
+        % (settings.USERNAME_VALIDATION_RE,),
         'grant_leader', name='kg_grant_leader'),
-    url(r'^(?P<project_id>%s)/revoke/(?P<username>%s)/$'
-        % (settings.USERNAME_VALIDATION_RE, settings.PROJECT_VALIDATION_RE),
+    url(r'^(?P<project_id>\d+)/revoke/(?P<username>%s)/$'
+        % (settings.USERNAME_VALIDATION_RE,),
         'revoke_leader', name='kg_revoke_leader'),
-    url(r'^(?P<project_id>%s)/logs/$' % settings.PROJECT_VALIDATION_RE,
+    url(r'^(?P<project_id>\d+)/logs/$',
         'project_logs', name='kg_project_logs'),
-    url(r'^(?P<project_id>%s)/add_comment/$' % settings.PROJECT_VALIDATION_RE,
+    url(r'^(?P<project_id>\d+)/add_comment/$',
         'add_comment', name='kg_project_add_comment'),
-    url(r'^(?P<project_id>%s)/quota/add/$' % settings.PROJECT_VALIDATION_RE,
+    url(r'^(?P<project_id>\d+)/quota/add/$',
         'projectquota_add', name='kg_projectquota_add'),
 )
 

karaage/projects/views.py
@@ -82,10 +82,11 @@ def add_edit_project(request, project_id=None):
         flag = 1
     else:
         kwargs['instance'] = project = \
-            get_object_or_404(Project, pid=project_id)
+            get_object_or_404(Project, id=project_id)
         kwargs['initial'] = {
             'leaders': list(project.leaders.values_list('pk', flat=True)),
         }
+        old_pid = project.pid
         flag = 2
 
     if request.method == 'POST':
@@ -102,9 +103,8 @@ def add_edit_project(request, project_id=None):
         if form.is_valid():
             project = form.save(commit=False)
             if project_id is not None:
-                # if project is being edited, project_id cannot change, so we
-                # should always use the value supplied on the URL.
-                project.pid = project_id
+                # if project is being edited, project_id cannot change.
+                project.pid = old_pid
             elif not project.pid:
                 # if project was being created, did the user give a project_id
                 # we should use? If not, then we have to generate one
@@ -143,7 +143,7 @@ def add_edit_project(request, project_id=None):
 @admin_required
 def delete_project(request, project_id):
 
-    project = get_object_or_404(Project, pid=project_id)
+    project = get_object_or_404(Project, id=project_id)
 
     query = Account.objects.filter(
         date_deleted__isnull=True, default_project=project)
@@ -170,7 +170,7 @@ def delete_project(request, project_id):
 @login_required
 def project_detail(request, project_id):
 
-    project = get_object_or_404(Project, pid=project_id)
+    project = get_object_or_404(Project, id=project_id)
     allocations = Allocation.objects.filter(grant__project=project)
     if not project.can_view(request):
         return HttpResponseForbidden('<h1>Access Denied</h1>')
@@ -200,7 +200,7 @@ def project_detail(request, project_id):
 
 @admin_required
 def project_verbose(request, project_id):
-    project = get_object_or_404(Project, pid=project_id)
+    project = get_object_or_404(Project, id=project_id)
 
     from karaage.datastores import machine_category_get_project_details
     project_details = machine_category_get_project_details(project)
@@ -249,7 +249,7 @@ def project_list(request, queryset=None):
 @login_required
 def remove_user(request, project_id, username):
 
-    project = get_object_or_404(Project, pid=project_id)
+    project = get_object_or_404(Project, id=project_id)
     person = get_object_or_404(Person, username=username)
 
     if not project.can_edit(request):
@@ -281,7 +281,7 @@ def remove_user(request, project_id, username):
 
 @login_required
 def grant_leader(request, project_id, username):
-    project = get_object_or_404(Project, pid=project_id)
+    project = get_object_or_404(Project, id=project_id)
     person = get_object_or_404(Person, username=username)
 
     if not project.can_edit(request):
@@ -307,7 +307,7 @@ def grant_leader(request, project_id, username):
 
 @login_required
 def revoke_leader(request, project_id, username):
-    project = get_object_or_404(Project, pid=project_id)
+    project = get_object_or_404(Project, id=project_id)
     person = get_object_or_404(Person, username=username)
 
     if not project.can_edit(request):
@@ -338,37 +338,37 @@ def no_users(request):
     project_ids = []
     for p in Project.active.all():
         if p.group.members.count() == 0:
-            project_ids.append(p.pid)
+            project_ids.append(p.id)
 
-    return project_list(request, Project.objects.filter(pid__in=project_ids))
+    return project_list(request, Project.objects.filter(id__in=project_ids))
 
 
 @admin_required
 def project_logs(request, project_id):
-    obj = get_object_or_404(Project, pid=project_id)
+    obj = get_object_or_404(Project, id=project_id)
     breadcrumbs = []
     breadcrumbs.append(
         ("Projects", reverse("kg_project_list")))
     breadcrumbs.append(
-        (six.text_type(obj.pid), reverse("kg_project_detail", args=[obj.pid])))
+        (six.text_type(obj.pid), reverse("kg_project_detail", args=[obj.id])))
     return util.log_list(request, breadcrumbs, obj)
 
 
 @admin_required
 def add_comment(request, project_id):
-    obj = get_object_or_404(Project, pid=project_id)
+    obj = get_object_or_404(Project, id=project_id)
     breadcrumbs = []
     breadcrumbs.append(
         ("Projects", reverse("kg_project_list")))
     breadcrumbs.append(
-        (six.text_type(obj.pid), reverse("kg_project_detail", args=[obj.pid])))
+        (six.text_type(obj.pid), reverse("kg_project_detail", args=[obj.id])))
     return util.add_comment(request, breadcrumbs, obj)
 
 
 @admin_required
 def projectquota_add(request, project_id):
 
-    project = get_object_or_404(Project, pid=project_id)
+    project = get_object_or_404(Project, id=project_id)
 
     project_chunk = ProjectQuota()
     project_chunk.project = project

karaage/templates/karaage/allocations/allocation_add_edit_template.html
@@ -8,7 +8,7 @@
 <div class="breadcrumbs">
     <a href='{% url "index" %}'>Home</a>&nbsp;›
     <a href="{% url 'kg_project_list' %}">Projects</a>&nbsp;›
-    <a href="{% url 'kg_project_detail' pid %}">{{ pid }}</a>&nbsp;›
+    <a href="{% url 'kg_project_detail' project.id %}">{{ project.pid }}</a>&nbsp;›
     {{ title }}
 </div>
 {% endblock %}
@@ -24,9 +24,9 @@
     {% ifequal mode 'edit' %}
         &nbsp;&nbsp;&nbsp;
         {% if record_type == 'grant'  %}
-            <a class="deletelink" href={% url 'grant_delete' pid record_id %}>Delete grant</a>
+            <a class="deletelink" href={% url 'grant_delete' project.id record_id %}>Delete grant</a>
         {% elif record_type == 'allocation' %}
-            <a class="deletelink" href={% url 'allocation_delete' pid record_id %}>Delete allocation</a>
+            <a class="deletelink" href={% url 'allocation_delete' project.id record_id %}>Delete allocation</a>
         {% endif %}
     {% endifequal %}
     </form>

karaage/templates/karaage/machines/account_detail.html
@@ -112,7 +112,7 @@
                 {% if is_admin %}
                 <td>
                     {% ifnotequal p account.default_project %}
-                    <a href="{% url 'kg_remove_project_member' p.pid account.person.username %}" class="deletelink">Remove</a>
+                    <a href="{% url 'kg_remove_project_member' p.id account.person.username %}" class="deletelink">Remove</a>
                     {% endifnotequal %}
                 </td>
                 {% endif %}

commit 72c7ff961ecff64b20f5f1e327654f2994c18d26

Make PID column in project table clickable.
This was broken in:
248e29877007dce1fcc2385057fe413f795df013

karaage/projects/tables.py
@@ -76,9 +76,8 @@ class ProjectFilter(django_filters.FilterSet):
 
 class ProjectTable(tables.Table):
     is_active = tables.Column(order_by=('-is_active'), verbose_name="active")
-    id = tables.LinkColumn(
-        'kg_project_detail', args=[A('id')], verbose_name="ID")
-    pid = tables.Column(verbose_name="PID")
+    pid = tables.LinkColumn(
+        'kg_project_detail', args=[A('id')], verbose_name="PID")
     institute = tables.LinkColumn(
         'kg_institute_detail', args=[A('institute.pk')])
     leaders = PeopleColumn(orderable=False)