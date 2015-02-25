5558119d155de9e3629c6940adb84f25c9920aac
Brian May <brian@microcomaustralia.com.au>
2015-02-25 12:04:26 +1100

    Add career level table
    Keep track of a career level for every person.
    
    Change-Id: I9d42d8c243d0a055b6b132a8ca61e9835257fb20

---
+++ karaage/migrations/0004_auto_karage4.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+import audit_log.models.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0001_initial'),
+        ('karaage', '0003_unique_groups'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='CareerLevel',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('level', models.CharField(max_length=255)),
+            ],
+            options={
+                'ordering': ['level'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='CareerLevelAuditLogEntry',
+            fields=[
+                ('id', models.IntegerField(auto_created=True, blank=True, verbose_name='ID', db_index=True)),
+                ('level', models.CharField(max_length=255)),
+                ('action_id', models.AutoField(serialize=False, primary_key=True)),
+                ('action_date', models.DateTimeField(default=django.utils.timezone.now, editable=False)),
+                ('action_type', models.CharField(max_length=1, choices=[('I', 'Created'), ('U', 'Changed'), ('D', 'Deleted')], editable=False)),
+                ('action_user', audit_log.models.fields.LastUserField(to='karaage.Person', null=True, editable=False, related_name='_careerlevel_audit_log_entry')),
+            ],
+            options={
+                'ordering': ('-action_date',),
+                'default_permissions': (),
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='person',
+            name='career_level',
+            field=models.ForeignKey(null=True, to='karaage.CareerLevel'),
+            preserve_default=True,
+        ),
+    ]
