fae218bf10f612b001793f227abbceaff6e42a26
Brian May <brian@microcomaustralia.com.au>
2015-02-25 16:30:04 +1100

    Add ProjectLevel table.
    Not used used anywhere.
    
    Change-Id: I8858fead0cf0a053498ba5a52c47afbd52c562b1

--- karaage/migrations/0004_auto_karage4.py
+++ karaage/migrations/0004_auto_karage4.py
@@ -41,6 +41,33 @@ class Migration(migrations.Migration):
             },
             bases=(models.Model,),
         ),
+        migrations.CreateModel(
+            name='ProjectLevelAuditLogEntry',
+            fields=[
+                ('id', models.IntegerField(auto_created=True, blank=True, verbose_name='ID', db_index=True)),
+                ('level', models.CharField(max_length=255)),
+                ('action_id', models.AutoField(serialize=False, primary_key=True)),
+                ('action_date', models.DateTimeField(default=django.utils.timezone.now, editable=False)),
+                ('action_type', models.CharField(max_length=1, choices=[('I', 'Created'), ('U', 'Changed'), ('D', 'Deleted')], editable=False)),
+                ('action_user', audit_log.models.fields.LastUserField(to='karaage.Person', null=True, editable=False, related_name='_projectlevel_audit_log_entry')),
+            ],
+            options={
+                'ordering': ('-action_date',),
+                'default_permissions': (),
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ProjectLevel',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('level', models.CharField(max_length=255)),
+            ],
+            options={
+                'ordering': ['level'],
+            },
+            bases=(models.Model,),
+        ),
         migrations.AddField(
             model_name='person',
             name='career_level',
