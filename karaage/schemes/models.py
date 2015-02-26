fc2d93e7c03717b0603bf29511e2ca2e3a49e4e6
Brian May <brian@microcomaustralia.com.au>
2015-02-26 13:32:16 +1100

    Add Grant Schemes.
    Change-Id: I0974c29a693b51c6614b86a45141b80784eeaa1d

---
+++ karaage/schemes/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.db.models import Q
+
+from audit_log.models.managers import AuditLog
+
+
+class ActiveSchemeManager(models.Manager):
+    def get_queryset(self):
+        query = super(ActiveSchemeManager, self).get_queryset()
+        return query.filter(closed=None)
+
+
+class DeletedSchemeManager(models.Manager):
+    def get_queryset(self):
+        query = super(DeletedSchemeManager, self).get_queryset()
+        return query.filter(~Q(closed=None))
+
+
+class Scheme(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=200, null=False, blank=True)
+    opened = models.DateField()
+    closed = models.DateField(null=True, blank=True)
+
+    objects = models.Manager()
+    active = ActiveSchemeManager()
+    deleted = DeletedSchemeManager()
+
+    audit_log = AuditLog()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
