89f8b0c2bb0dfbf7e7eb52fa335b40f5136e3ded
Brian May <brian@microcomaustralia.com.au>
2015-02-27 13:35:49 +1100

    Remove datastore choices from db model.
    This was being added to choices= to the migrations of the datastore
    Attribute of MachineCategory.
    
    As MACHINE_CATEGORY_DATASTORES is a dictionary, the order was
    non-determistic, which would change the value of the choices attribute,
    meaning makemigrations would falsely detect changes to the database
    schema.
    
    Change-Id: I415f6b9b5a348f173589419b4e9b0c7790ffb516

---
+++ karaage/migrations/0006_auto_20150317_1405.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('karaage', '0005_auto_20150303_1537'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='machinecategory',
+            name='datastore',
+            field=models.CharField(help_text='Modifying this value on existing categories will affect accounts created under the old datastore', max_length=255),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='machinecategoryauditlogentry',
+            name='datastore',
+            field=models.CharField(help_text='Modifying this value on existing categories will affect accounts created under the old datastore', max_length=255),
+            preserve_default=True,
+        ),
+    ]
