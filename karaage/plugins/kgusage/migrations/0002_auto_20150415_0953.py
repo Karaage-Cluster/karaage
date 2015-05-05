d7d8c6f870f0b79022a7abda32586b1555dfa114
Brian May <brian@microcomaustralia.com.au>
2015-05-05 13:57:47 +1000

    denormalize CpuJob table
    Required to keep track of the values when the job was run, as opposed to
    the values when querying the database.
    
    Change-Id: I712b662a1029cbfbd69d31eea49fb6bdf1a183b5

---
+++ karaage/plugins/kgusage/migrations/0002_auto_20150415_0953.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('karaage', '0005_auto_karage4'),
+        ('kgusage', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='cpujob',
+            name='person_career_level',
+            field=models.ForeignKey(to='karaage.CareerLevel', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='cpujob',
+            name='person_institute',
+            field=models.ForeignKey(related_name='person_institute_cpujob', to='karaage.Institute', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='cpujob',
+            name='person_project_level',
+            field=models.ForeignKey(to='karaage.ProjectLevel', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='cpujob',
+            name='project_institute',
+            field=models.ForeignKey(related_name='project_institute_cpujob', to='karaage.Institute', null=True),
+            preserve_default=True,
+        ),
+    ]
