bc39811c720e0edee7a050ebaa7db71798a05149
Brian May <brian@microcomaustralia.com.au>
2015-06-09 11:15:56 +1000

    Fix broken searches.
    Change-Id: I4d9f774630eedb919ef226d457fff671cd41cdbb

--- karaage/people/views/persons.py
+++ karaage/people/views/persons.py
@@ -121,7 +121,7 @@ def user_list(request, queryset=None, title=None):
 def locked_list(request):
 
     result = QueryDict("", mutable=True)
-    result['login_enabled'] = False
+    result['active'] = "locked"
     url = reverse('kg_person_list') + "?" + result.urlencode()
     return HttpResponseRedirect(url)
 
@@ -132,7 +132,7 @@ def struggling(request):
     days30 = today - datetime.timedelta(days=30)
 
     result = QueryDict("", mutable=True)
-    result['is_active'] = True
+    result['active'] = "yes"
     result['begin_date_approved'] = days30
     result['no_last_usage'] = True
     result['sort'] = "-date_approved"
