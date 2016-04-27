5a53c083206a22f170b79dbb23bf1fe0951d75b2
Brian May <brian@linuxpenguins.xyz>
2016-04-27 10:18:54 +1000

    Fix isnull comparison
    Change-Id: I2bb18121dc2c4adb973bd2319070e82b8909d7f4

--- karaage/people/tables.py
+++ karaage/people/tables.py
@@ -67,7 +67,8 @@ class PersonFilter(django_filters.FilterSet):
     username = django_filters.CharFilter(lookup_type="icontains")
     full_name = django_filters.CharFilter(lookup_type="icontains")
     email = django_filters.CharFilter(lookup_type="icontains")
-    no_last_usage = django_filters.BooleanFilter(name="last_usage__isnull")
+    no_last_usage = django_filters.BooleanFilter(
+        name="last_usage", lookup_expr="isnull")
     begin_last_usage = django_filters.DateFilter(
         name="last_usage", lookup_type="gte")
     end_last_usage = django_filters.DateFilter(
