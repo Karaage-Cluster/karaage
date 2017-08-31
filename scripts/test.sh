0737d0eb8d266e4f855f665b7e38a506ea6e5550
Brian May <brian@linuxpenguins.xyz>
2017-08-31 17:18:31 +1000

    Add testing script
    Change-Id: I7eb0574d0d6b2318517dcc8aad64350d5fb18453

---
+++ scripts/test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+/usr/bin/docker run -ti --name karaage \
+  --rm \
+  -p 8000:8000 \
+  -v /etc/passwd:/etc/passwd \
+  -v /etc/group:/etc/group \
+  -v $PWD/local/settings:/etc/karaage3 \
+  -v $PWD/local/log:/var/log/karaage3 \
+  brianmay/karaage:slurm16.05 "$@"
+
+# other desirable parameters
+#  --net="host" \
+#  -v /etc/munge:/etc/munge \
+#  -v /var/lib/munge:/var/lib/munge \
+#  -v /var/log/munge:/var/log/munge \
+#  -v /usr/local/slurm/etc:/usr/local/etc \
