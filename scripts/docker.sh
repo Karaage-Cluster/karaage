d58498a7077638ba552ed1d7ad8b3749a87705ec
Brian May <brian@linuxpenguins.xyz>
2017-08-31 17:18:03 +1000

    Make docker entry point script more robust
    Change-Id: I2fb0f0c9f7814f409e79fa07a410e58c2dc65e16

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 set -e
 echo "www-data ALL=(slurm) NOPASSWD: /usr/local/bin/sacct" >> /etc/sudoers
-service munge start
-mkdir /var/lib/karaage3
-chown www-data:www-data /var/lib/karaage3
-mkdir /var/cache/karaage3
-chown www-data:www-data /var/cache/karaage3
+
+if getent passwd munge > /dev/null
+then
+    service munge start
+fi
+
+if ! test -d /var/lib/karaage3
+then
+    mkdir /var/lib/karaage3
+    chown www-data:www-data /var/lib/karaage3
+fi
+
+if ! test -d /var/cache/karaage3
+then
+    mkdir /var/cache/karaage3
+    chown www-data:www-data /var/cache/karaage3
+fi
+
 sudo -u www-data ./scripts/start.sh
