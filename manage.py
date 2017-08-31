953913a3ffcf829d196c171f9a2ad21125332563
Brian May <brian@linuxpenguins.xyz>
2017-08-31 17:36:00 +1000

    Update manage.py
    Change-Id: I6da0112d87e2fe18e68afde34fa7b65079ff8056

--- manage.py
+++ manage.py
@@ -17,8 +17,27 @@
 # You should have received a copy of the GNU General Public License
 # along with Karaage  If not, see <http://www.gnu.org/licenses/>.
 import os
-from django.core import management
+import sys
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'karaage.conf.settings'
+
 if __name__ == "__main__":
-    management.execute_from_command_line()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'karaage.conf.settings')
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        execute_from_command_line = None
+        try:
+            import django  # pylint:disable=unused-import
+        except ImportError:
+            django = None
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
+    execute_from_command_line(sys.argv)
