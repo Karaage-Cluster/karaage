e605f1e1539db862c7c3161ece9dd64de3b6a16c
Brian May <brian@linuxpenguins.xyz>
2017-10-25 18:35:49 +1100

    Remove slurm files, now in seperate project
    Change-Id: I52ff062b8ca59bef3bc7266286991ad4093d8f61

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 set -e
 
-if test "$1" = "--build-slurm"
-then
-    shift
-    docker build \
-        --tag "brianmay/slurm:16.05" \
-        --build-arg="SLURM_VER=16.05.10" \
-        --build-arg="SLURM_URL=https://www.schedmd.com/downloads/archive/slurm-16.05.10.tar.bz2" \
-        slurm
-fi
-
 docker build \
     --file "Dockerfile" \
     --tag "brianmay/karaage:slurm16.05" \
