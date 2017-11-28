51f0eee1227a3437e15a842761fcfce8316a3e55
Brian May <brian@linuxpenguins.xyz>
2017-11-28 17:50:10 +1100

    Bump version: 3.1.33 → 3.1.34
    Change-Id: I831dfd81934c5b0cb3eabe3c50d74d635102671b

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ tests_require = [
 
 setup(
     name="karaage",
-    version='3.1.33',
+    version='3.1.34',
     url='https://github.com/Karaage-Cluster/karaage',
     author='Brian May',
     author_email='brian@v3.org.au',
