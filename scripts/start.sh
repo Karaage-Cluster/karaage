fd85ab2fb84ab47c201c1e26162df39de2edbf1c
Brian May <brian@linuxpenguins.xyz>
2018-02-27 17:47:10 +1100

    Correct pid filenames so they don't conflict
    Change-Id: I06036df8ecdb5aa4a9d4c902a9869a4b797ca695

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,13 +5,13 @@ python3 manage.py migrate --noinput
 python3 -m celery -A karaage.celery worker \
     --concurrency=1 \
     --loglevel=INFO \
-    --pidfile=/tmp/celeryd.pid \
+    --pidfile=/var/run/lock/celery.pid \
     --logfile=/var/log/karaage3/celery.log \
     --detach
 python3 -m celery -A karaage.celery beat \
     --schedule /tmp/celerybeat-schedule \
     --loglevel=INFO \
-    --pidfile=/tmp/celeryd.pid \
+    --pidfile=/var/run/lock/beat.pid \
     --logfile=/var/log/karaage3/beat.log \
     --detach
 
