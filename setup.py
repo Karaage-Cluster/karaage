38529d81afcd6ee0190ef5683f82f5ad74d01124
Brian May <brian@linuxpenguins.xyz>
2018-12-03 20:24:46 +1100

    Update supported Python versions to 3.6 and 3.7 only
    Change-Id: Ie9f7faf155a3936641c4182cafafe39ff5b9cb12

--- setup.py
+++ setup.py
@@ -74,9 +74,8 @@ setup(
         "License :: OSI Approved :: GNU General Public "
             "License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords="karaage cluster user administration",
