34b57e1da77167858d0d74a1fab6a0b31601ae55
Brian May <brian@linuxpenguins.xyz>
2019-03-12 18:45:15 +1100

    Update run_tests.sh
    Change-Id: I96ab7d86b49510bc87efb8b694a48e0a624d3d51

--- run_tests.sh
+++ run_tests.sh
@@ -4,12 +4,6 @@ DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 RETURN=0
 cd $DIR
 
-if [ -n "$*" ]; then
-    TESTS="$@"
-else
-    TESTS="karaage.tests"
-fi
-
 echo ""
 echo "ISORT"
 echo "############################"
@@ -28,39 +22,33 @@ then
     exit 1
 fi
 
-echo -e "\n\n"
+conf="karaage.tests.settings"
+echo ""
+echo "MIGRATIONS - $conf"
+echo "############################"
+./manage.py makemigrations --settings="$conf" --check --dry-run
+if [ "$?" -ne 0 ]
+then
+    exit 1
+fi
 
-for values in $TESTS; do
-    conf="karaage.tests.settings"
-    tests="$values"
+echo ""
+echo "STATIC FILES - $conf"
+echo "############################"
+rm -rf tmp
+./manage.py collectstatic --settings="$conf" -v 2 --noinput
+if [ "$?" -ne 0 ]
+then
+    exit 1
+fi
 
-    echo ""
-    echo "MIGRATIONS - $conf"
-    echo "############################"
-    ./manage.py makemigrations --settings="$conf" --check --dry-run
-    if [ "$?" -ne 0 ]
-    then
-        exit 1
-    fi
-
-    echo ""
-    echo "STATIC FILES - $conf"
-    echo "############################"
-    rm -rf tmp
-    ./manage.py collectstatic --settings="$conf" -v 2 --noinput
-    if [ "$?" -ne 0 ]
-    then
-        exit 1
-    fi
-
-    echo ""
-    echo "TESTS - Python 3 - $conf - $tests"
-    echo "############################"
-    python3 ./manage.py test --settings="$conf" -v 2 $tests
-    if [ "$?" -ne 0 ]
-    then
-        exit 1
-    fi
-done
+echo ""
+echo "TESTS - Python 3 - $conf - $tests"
+echo "############################"
+pytest --cov=karaage "$@"
+if [ "$?" -ne 0 ]
+then
+    exit 1
+fi
 
 exit "$RETURN"
