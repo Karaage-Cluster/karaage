a6c781121f5ef9cd21d11f75a5c22307c7a71b21
Brian May <brian@linuxpenguins.xyz>
2019-06-05 08:33:40 +1000

    Merge in changes to extract_views_from_urlpatterns from source
    The extract_views_from_urlpattern comes from
    django_extensions/management/commands/show_urls.py
    
    This function was broken with Django 2.2, which broke all tests
    relying on TestAllPagesCase.
    
    Change-Id: Iad84a8432c26a25d9e5655d669e570710a9b7342

--- karaage/tests/client.py
+++ karaage/tests/client.py
@@ -28,11 +28,7 @@ from django.conf import settings
 from django.contrib.admindocs.views import simplify_regex
 from django.core.exceptions import ViewDoesNotExist
 from django.test import TestCase
-from django.urls import (
-    LocaleRegexURLResolver,
-    RegexURLPattern,
-    RegexURLResolver,
-)
+from django.urls import URLPattern, URLResolver
 from django.utils import translation
 from django.utils.encoding import smart_text
 from django.utils.text import slugify
@@ -43,17 +39,33 @@ from karaage.middleware.threadlocals import reset
 urlconf = __import__(settings.ROOT_URLCONF, {}, {}, [''])
 
 
+class RegexURLPattern:  # type: ignore
+    pass
+
+
+class RegexURLResolver:  # type: ignore
+    pass
+
+
+class LocaleRegexURLResolver:  # type: ignore
+    pass
+
+
+def describe_pattern(p):
+    return str(p.pattern)
+
+
 def extract_views_from_urlpatterns(urlpatterns, base='', namespace=None):
     """
     Return a list of views from a list of urlpatterns.
 
-    Each object in the returned list is a two-tuple: (view_func, regex)
+    Each object in the returned list is a three-tuple: (view_func, regex, name)
     """
     LANGUAGES = getattr(settings, 'LANGUAGES', ((None, None), ))
 
     views = []
     for p in urlpatterns:
-        if isinstance(p, RegexURLPattern):
+        if isinstance(p, (URLPattern, RegexURLPattern)):
             try:
                 if not p.name:
                     name = p.name
@@ -61,10 +73,11 @@ def extract_views_from_urlpatterns(urlpatterns, base='', namespace=None):
                     name = '{0}:{1}'.format(namespace, p.name)
                 else:
                     name = p.name
-                views.append((p.callback, base + p.regex.pattern, name))
+                pattern = describe_pattern(p)
+                views.append((p.callback, base + pattern, name))
             except ViewDoesNotExist:
                 continue
-        elif isinstance(p, RegexURLResolver):
+        elif isinstance(p, (URLResolver, RegexURLResolver)):
             try:
                 patterns = p.url_patterns
             except ImportError:
@@ -73,20 +86,16 @@ def extract_views_from_urlpatterns(urlpatterns, base='', namespace=None):
                 _namespace = '{0}:{1}'.format(namespace, p.namespace)
             else:
                 _namespace = (p.namespace or namespace)
+            pattern = describe_pattern(p)
             if isinstance(p, LocaleRegexURLResolver):
-                for langauge in LANGUAGES:
-                    with translation.override(langauge[0]):
-                        views.extend(
-                            extract_views_from_urlpatterns(
-                                patterns, base + p.regex.pattern,
-                                namespace=_namespace))
+                for language in LANGUAGES:
+                    with translation.override(language[0]):
+                        views.extend(extract_views_from_urlpatterns(patterns, base + pattern, namespace=_namespace))
             else:
-                views.extend(extract_views_from_urlpatterns(
-                    patterns, base + p.regex.pattern, namespace=_namespace))
+                views.extend(extract_views_from_urlpatterns(patterns, base + pattern, namespace=_namespace))
         elif hasattr(p, '_get_callback'):
             try:
-                views.append(
-                    (p._get_callback(), base + p.regex.pattern, p.name))
+                views.append((p._get_callback(), base + describe_pattern(p), p.name))
             except ViewDoesNotExist:
                 continue
         elif hasattr(p, 'url_patterns') or hasattr(p, '_get_url_patterns'):
@@ -94,12 +103,9 @@ def extract_views_from_urlpatterns(urlpatterns, base='', namespace=None):
                 patterns = p.url_patterns
             except ImportError:
                 continue
-            views.extend(
-                extract_views_from_urlpatterns(
-                    patterns, base + p.regex.pattern, namespace=namespace))
+            views.extend(extract_views_from_urlpatterns(patterns, base + describe_pattern(p), namespace=namespace))
         else:
-            raise TypeError(
-                "%s does not appear to be a urlpattern object" % (p,))
+            raise TypeError("%s does not appear to be a urlpattern object" % p)
     return views
 
 
