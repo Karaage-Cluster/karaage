224b068dd09219a13d5d532ee5b7f4b8ac97bab2
Brian May <brian@linuxpenguins.xyz>
2019-06-05 17:36:49 +1000

    Remove declaration of Python 3.6 support
    Change-Id: I05fe66709e3128d04f8758874997710124380f0b

--- setup.py
+++ setup.py
@@ -104,7 +104,6 @@ setup(
         "License :: OSI Approved :: GNU General Public "
             "License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
