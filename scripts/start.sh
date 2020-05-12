9f3786741190f47b5ce24ab9c311f98118db3eb1
Brian May <brian@linuxpenguins.xyz>
2020-05-13 07:01:28 +1000

    Add useful parameters to gunicorn

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,5 +6,7 @@ python3 manage.py migrate --noinput
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn karaage.wsgi:application \
+    --log-file=- \
     --bind 0.0.0.0:8000 \
+    --worker-tmp-dir /dev/shm \
     --workers 3
