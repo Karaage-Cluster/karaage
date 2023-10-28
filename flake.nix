1123b877725114066ad1bc9dd1716375f625b028
Brian May <brian@linuxpenguins.xyz>
2023-10-28 12:45:04 +1100

    Build now works

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Cluster account management";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.poetry2nix = {
     url = "github:nix-community/poetry2nix";
     inputs.nixpkgs.follows = "nixpkgs";
@@ -11,8 +11,8 @@
   outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
+        p2n = import poetry2nix { inherit pkgs; };
+        mkPoetryApplication = p2n.mkPoetryApplication;
         pkgs = nixpkgs.legacyPackages.${system};
         slapd = pkgs.writeShellScriptBin "slapd" ''
           exec ${pkgs.openldap}/libexec/slapd "$@"
@@ -20,13 +20,41 @@
 
       in {
         packages = {
-          karaage = mkPoetryApplication { projectDir = self; };
+          karaage = mkPoetryApplication {
+            projectDir = self;
+            overrides = p2n.overrides.withDefaults (final: prev: {
+              #nh3 = prev.nh3.override { preferWheel = true; };
+              furo = prev.furo.override { preferWheel = true; };
+              cracklib = prev.cracklib.overridePythonAttrs (oldattrs: {
+                buildInputs = oldattrs.buildInputs
+                  ++ [ final.setuptools pkgs.cracklib ];
+              });
+              django-filter = prev.django-filter.overridePythonAttrs
+                (oldattrs: {
+                  buildInputs = oldattrs.buildInputs ++ [ final.flit-core ];
+                });
+              django-ajax-selects = prev.django-ajax-selects.overridePythonAttrs
+                (oldattrs: {
+                  buildInputs = oldattrs.buildInputs ++ [ final.setuptools ];
+                });
+              python-alogger = prev.python-alogger.overridePythonAttrs
+                (oldattrs: {
+                  buildInputs = oldattrs.buildInputs ++ [ final.setuptools ];
+                });
+              python-tldap = prev.python-tldap.overridePythonAttrs (oldattrs: {
+                buildInputs = oldattrs.buildInputs ++ [ final.poetry-core ];
+              });
+              bump2version = prev.bump2version.overridePythonAttrs (oldAttrs: {
+                buildInputs = oldAttrs.buildInputs ++ [ final.setuptools ];
+              });
+            });
+          };
           default = self.packages.${system}.karaage;
         };
 
         devShells.default = pkgs.mkShell {
           packages = [
-            poetry2nix.packages.${system}.poetry
+            pkgs.poetry
             pkgs.libffi
             slapd
             pkgs.openldap
