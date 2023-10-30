0a7620389abb3dc9ddbb6bbd0516ae7fe7ada94f
Brian May <brian@linuxpenguins.xyz>
2023-10-31 09:28:23 +1100

    Fix type in env KARAAGE_CONFIG_FILE

--- karaage/cli.py
+++ karaage/cli.py
@@ -3,6 +3,6 @@ import sys
 from django.core.management import execute_from_command_line
 
 def manage():
-    os.environ.setdefault('KARAAGE_CONF', '/etc/karaage3/settings.py')
+    os.environ.setdefault('KARAAGE_CONFIG_FILE', '/etc/karaage3/settings.py')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'karaage.conf.settings')
     execute_from_command_line(sys.argv)
