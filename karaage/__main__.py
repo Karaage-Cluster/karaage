c4ef444cc7edb986e9725fd68a1709d93f2c9221
Brian May <brian@linuxpenguins.xyz>
2023-10-31 19:05:34 +1100

    Fix formatting

--- karaage/__main__.py
+++ karaage/__main__.py
@@ -1,2 +1,3 @@
 from karaage.cli import manage
+
 manage()
--- karaage/cli.py
+++ karaage/cli.py
@@ -2,7 +2,8 @@ import os
 import sys
 from django.core.management import execute_from_command_line
 
+
 def manage():
-    os.environ.setdefault('KARAAGE_CONFIG_FILE', '/etc/karaage3/settings.py')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'karaage.conf.settings')
+    os.environ.setdefault("KARAAGE_CONFIG_FILE", "/etc/karaage3/settings.py")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "karaage.conf.settings")
     execute_from_command_line(sys.argv)
