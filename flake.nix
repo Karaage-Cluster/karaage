c85d8b23a7bdb3a607b18e4a2019e6634989ebc6
Brian May <brian@linuxpenguins.xyz>
2024-07-16 09:01:04 +1000

    Update nix flakes

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -39,7 +39,8 @@
                 });
               django-ajax-selects = prev.django-ajax-selects.overridePythonAttrs
                 (oldattrs: {
-                  buildInputs = oldattrs.buildInputs ++ [ final.setuptools ];
+                  buildInputs = oldattrs.buildInputs
+                    ++ [ final.setuptools final.poetry ];
                 });
               python-alogger = prev.python-alogger.overridePythonAttrs
                 (oldattrs: {
